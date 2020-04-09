69b08f20a7a8dabd026bcc311e06872d2f954f3d
Luke Hill <lukehill_uk@hotmail.com>
2020-04-09 16:42:59 +0200

    initial commit

---
+++ lcd/lib/lcd.rb
@@ -0,0 +1,113 @@
+class LCD
+  attr_reader :input, :width, :height
+
+  def initialize(input, width: 1, height: 1)
+    @input = input
+    @width = width
+    @height = height
+  end
+
+  def render
+    "#{render_line_one}\n"\
+    "#{render_height_between_lines_one_and_two}"\
+    "#{render_line_two}\n"\
+    "#{render_height_between_lines_two_and_three}"\
+    "#{render_line_three}\n"
+  end
+
+  private
+
+  def render_height_between_lines_one_and_two
+    spaces = ' ' * width
+    one_line = number_as_digits.map { |digit| first_height_extender(digit, spaces) }.join
+
+    "#{one_line}\n" * (height - 1)
+  end
+
+  def render_height_between_lines_two_and_three
+    spaces = ' ' * width
+    one_line = number_as_digits.map { |digit| second_height_extender(digit, spaces) }.join
+
+    "#{one_line}\n" * (height - 1)
+  end
+
+  def render_line_one
+    number_as_digits.map { |digit| line_one(digit) }.join
+  end
+
+  def render_line_two
+    number_as_digits.map { |digit| line_two(digit) }.join
+  end
+
+  def render_line_three
+    number_as_digits.map { |digit| line_three(digit) }.join
+  end
+
+  def number_as_digits
+    if input.is_a?(Integer)
+      input.digits.reverse
+    else
+      input.each_char
+    end
+  end
+
+  def line_one(digit)
+    " #{underscore(digit, 0)} "
+  end
+  
+  def first_height_extender(digit, spaces)
+    "#{pipe(digit, 1)}#{spaces}#{pipe(digit, 3)}"
+  end
+
+  def line_two(digit)
+    "#{pipe(digit, 1)}#{underscore(digit, 2)}#{pipe(digit, 3)}"
+  end
+
+  def second_height_extender(digit, spaces)
+    "#{pipe(digit, 4)}#{spaces}#{pipe(digit, 6)}"
+  end
+
+  def line_three(digit)
+    "#{pipe(digit, 4)}#{underscore(digit, 5)}#{pipe(digit, 6)}"
+  end
+
+  def underscore(digit, position)
+    if write_character?(digit, position)
+      '_' * width
+    else
+      ' ' * width
+    end
+  end
+
+  def pipe(digit, position)
+    if write_character?(digit, position)
+      '|'
+    else
+      ' '
+    end
+  end
+
+  def write_character?(digit, position)
+    lcd_table[digit].include?(position)
+  end
+
+  def lcd_table #Top,Mid-Left,Mid,Mid-Right,Bot-Left,Bot,Bot-Right
+    {
+      1 => [3, 6],
+      2 => [0, 2, 3, 4, 5],
+      3 => [0, 2, 3, 5, 6],
+      4 => [1, 2, 3, 6],
+      5 => [0, 1, 2, 5, 6],
+      6 => [0, 1, 2, 4, 5, 6],
+      7 => [0, 3, 6],
+      8 => [0, 1, 2, 3, 4, 5, 6],
+      9 => [0, 1, 2, 3, 5, 6],
+      'a' => [0, 2, 3, 4, 5, 6],
+      'b' => [1, 2, 4, 5, 6],
+      'c' => [2, 4, 5],
+      'd' => [2, 3, 4, 5, 6],
+      'e' => [0, 1, 2, 4, 5],
+      'f' => [0, 1, 2, 4],
+    }
+  end
+end
---
+++ lcd/spec/lcd_spec.rb
@@ -0,0 +1,199 @@
+require 'lcd'
+
+describe 'LCD' do
+  context 'without parameter modifications' do
+    context 'viewing a single digit' do
+      let(:six) { LCD.new(6) }
+      let(:eight) { LCD.new(8) }
+
+      it 'should render a 6' do
+        expected = <<~SQUIGGLY_HEREDOC
+           _ 
+          |_ 
+          |_|
+        SQUIGGLY_HEREDOC
+
+        expect( six.render ).to eq( expected )
+      end
+
+      it 'should render an 8' do
+        expected = <<~SQUIGGLY_HEREDOC
+           _ 
+          |_|
+          |_|
+        SQUIGGLY_HEREDOC
+
+        expect( eight.render ).to eq( expected )
+      end
+    end
+
+    context 'viewing multiple digits' do
+      let(:four_eight_seven) { LCD.new(487) }
+      let(:nine_nine_one_three) { LCD.new(9913) }
+
+      it 'should render 487' do
+        expected = <<~SQUIGGLY_HEREDOC
+              _  _ 
+          |_||_|  |
+            ||_|  |
+        SQUIGGLY_HEREDOC
+
+        expect( four_eight_seven.render ).to eq( expected )
+      end
+
+      it 'should render 9913' do
+        expected = <<~SQUIGGLY_HEREDOC
+           _  _     _ 
+          |_||_|  | _|
+           _| _|  | _|
+        SQUIGGLY_HEREDOC
+
+        expect( nine_nine_one_three.render ).to eq( expected )
+      end
+    end
+
+    context 'viewing multiple lowercase letters from a-f' do
+      let(:abc) { LCD.new('abc') }
+      let(:fed) { LCD.new('fed') }
+
+      it 'should render abc' do
+        expected = <<~SQUIGGLY_HEREDOC
+           _       
+           _||_  _ 
+          |_||_||_ 
+        SQUIGGLY_HEREDOC
+
+        expect( abc.render ).to eq( expected )
+      end
+
+      it 'should render fed' do
+        expected = <<~SQUIGGLY_HEREDOC
+           _  _    
+          |_ |_  _|
+          |  |_ |_|
+        SQUIGGLY_HEREDOC
+
+        expect( fed.render ).to eq( expected )
+      end
+    end
+  end
+
+  context 'with parameter modifications' do
+    context 'viewing digits with modified width' do
+      let(:eight) { LCD.new(8, width: 2) }
+      let(:nine_one_three) { LCD.new(913, width: 4) }
+
+      it 'should render a widened 8' do
+        expected = <<~SQUIGGLY_HEREDOC
+           __ 
+          |__|
+          |__|
+        SQUIGGLY_HEREDOC
+
+        expect( eight.render ).to eq( expected )
+      end
+
+      it 'should render a widened 913' do
+        expected = <<~SQUIGGLY_HEREDOC
+           ____        ____ 
+          |____|     | ____|
+           ____|     | ____|
+        SQUIGGLY_HEREDOC
+
+        expect( nine_one_three.render ).to eq( expected )
+      end
+    end
+
+    context 'viewing digits with modified height' do
+      let(:two) { LCD.new(2, height: 3) }
+      let(:eight_seven) { LCD.new(87, height: 4) }
+
+      it 'should render a heightened 2' do
+        expected = <<~SQUIGGLY_HEREDOC
+          _ 
+           |
+           |
+          _|
+         |  
+         |  
+         |_ 
+        SQUIGGLY_HEREDOC
+
+        expect( two.render ).to eq( expected )
+      end
+
+      it 'should render a heightened 87' do
+        expected = <<~SQUIGGLY_HEREDOC
+            _  _ 
+           | |  |
+           | |  |
+           | |  |
+           |_|  |
+           | |  |
+           | |  |
+           | |  |
+           |_|  |
+        SQUIGGLY_HEREDOC
+
+        expect( eight_seven.render ).to eq( expected )
+      end
+    end
+
+    context 'viewing digits with modified width AND height' do
+      let(:eight) { LCD.new(8, height: 3, width: 2) }
+      let(:four_seven_one_eight_nine) { LCD.new(47189, height: 5, width: 7) }
+
+      it 'should render a heightened AND widened 8' do
+        expected = <<~SQUIGGLY_HEREDOC
+         __ 
+        |  |
+        |  |
+        |__|
+        |  |
+        |  |
+        |__|
+        SQUIGGLY_HEREDOC
+
+        expect( eight.render ).to eq( expected )
+      end
+
+      it 'should render a heightened AND widened 47189' do
+        expected = <<~SQUIGGLY_HEREDOC
+                  _______           _______  _______ 
+        |       |        |        ||       ||       |
+        |       |        |        ||       ||       |
+        |       |        |        ||       ||       |
+        |       |        |        ||       ||       |
+        |_______|        |        ||_______||_______|
+                |        |        ||       |        |
+                |        |        ||       |        |
+                |        |        ||       |        |
+                |        |        ||       |        |
+                |        |        ||_______| _______|
+        SQUIGGLY_HEREDOC
+
+        expect( four_seven_one_eight_nine.render ).to eq( expected )
+      end
+    end
+
+    context 'viewing multiple lowercase letters from a-f with modified width AND height' do
+      let(:abcdef) { LCD.new('abcdef', height: 4, width: 6) }
+
+      it 'should render a heightened AND widened abcdef' do
+        expected = <<~SQUIGGLY_HEREDOC
+        ______                          ______  ______ 
+              ||                      ||       |       
+              ||                      ||       |       
+              ||                      ||       |       
+        ______||______  ______  ______||______ |______ 
+       |      ||      ||       |      ||       |       
+       |      ||      ||       |      ||       |       
+       |      ||      ||       |      ||       |       
+       |______||______||______ |______||______ |       
+        SQUIGGLY_HEREDOC
+
+        expect( abcdef.render ).to eq( expected )
+      end
+    end
+  end
+end
