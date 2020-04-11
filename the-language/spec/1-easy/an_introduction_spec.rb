816312252bea3a77b2670dcd1baea1fc8f89d74d
Malte Gergeleit <malte.gergeleit@outlook.com>
2020-04-11 16:13:22 +0200

    First Commit

--- the-language/spec/1-easy/an_introduction_spec.rb
+++ the-language/spec/1-easy/an_introduction_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'Learning ruby' do
     end
 
     it 'verifies our expectations against reality' do
-      expected = __
+      expected = 2
       actual   = 1 + 1
 
       expect(actual).to be expected
