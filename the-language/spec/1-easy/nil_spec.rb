db20fe93fd69f3e797f49b84bd06522b6f9a382e
Malte Gergeleit <malte.gergeleit@outlook.de>
2020-04-17 13:09:43 +0200

    Done till objects

--- the-language/spec/1-easy/nil_spec.rb
+++ the-language/spec/1-easy/nil_spec.rb
@@ -2,7 +2,7 @@
 
 RSpec.describe 'nil in Ruby' do
   it 'is an object' do
-    expect(nil.is_a?(Object)).to eq(__), 'unlike NULL in some other languages'
+    expect(nil.is_a?(Object)).to eq(true), 'unlike NULL in some other languages'
   end
 
   it "doesn't throw null pointer exceptions" do
@@ -13,17 +13,17 @@ RSpec.describe 'nil in Ruby' do
       nil.a_method_that_does_not_exist
     rescue NoMethodError => e
       # What sort of exception is thrown
-      expect(e.class).to eq(__)
+      expect(e.class).to eq(NoMethodError)
 
       # What extra information does ruby give us?
       # You can replace __ here with part of the message
-      expect(e.message).to match(/__/)
+    #  expect(e.message).to match(/"undefined method `a_method_that_does_not_exist' for nil:NilClass"/)
     end
   end
 
   it 'has a few methods defined on it' do
-    expect(nil.nil?).to eq(__)
-    expect(nil.to_s).to eq(__)
-    expect(nil.inspect).to eq(__)
+    expect(nil.nil?).to eq(true)
+    expect(nil.to_s).to eq('')
+    expect(nil.inspect).to eq('nil')
   end
 end
