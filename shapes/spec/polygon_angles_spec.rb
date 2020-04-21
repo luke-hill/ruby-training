21f27642932d27dd34319d4f7eaae9c68fbe43f4
Luke Hill <lukehill_uk@hotmail.com>
2020-04-21 14:05:31 +0200

    Shapes fixups

--- shapes/spec/polygon_angles_spec.rb
+++ shapes/spec/polygon_angles_spec.rb
@@ -3,55 +3,55 @@
 require 'polygon_angles' # RSpec automatically adds `lib/` to the LOAD_PATH
 # edit lib/polygon_angles.rb to fix these tests
 
-RSpec.describe 'polygon angles types' do
-  context 'interior angles' do
-    it 'it calculates the interior angle of a regular pentagon' do
+RSpec.describe 'polygon angle calculation' do
+  context 'with interior angles' do
+    it 'calculates the interior angle of a regular pentagon' do
       expect(internal_angle(5)).to eq(108)
     end
 
-    it 'it calculates the interior angle of a regular hexagon' do
+    it 'calculates the interior angle of a regular hexagon' do
       expect(internal_angle(6)).to eq(120)
     end
 
-    it 'it calculates the interior angle of a regular heptagon' do
+    it 'calculates the interior angle of a regular heptagon' do
       expect(internal_angle(7)).to eq(128.57)
     end
 
-    it 'it calculates the interior angle of a regular octagon' do
+    it 'calculates the interior angle of a regular octagon' do
       expect(internal_angle(8)).to eq(135)
     end
 
-    it 'it calculates the interior angle of a regular nonagon' do
+    it 'calculates the interior angle of a regular nonagon' do
       expect(internal_angle(9)).to eq(140)
     end
 
-    it 'it calculates the interior angle of a regular decagon' do
+    it 'calculates the interior angle of a regular decagon' do
       expect(internal_angle(10)).to eq(144)
     end
   end
 
-  context 'exterior angles' do
-    it 'it calculates the exterior angle of a regular pentagon' do
+  context 'with exterior angles' do
+    it 'calculates the exterior angle of a regular pentagon' do
       expect(external_angle(5)).to eq(72)
     end
 
-    it 'it calculates the exterior angle of a regular hexagon' do
+    it 'calculates the exterior angle of a regular hexagon' do
       expect(external_angle(6)).to eq(60)
     end
 
-    it 'it calculates the exterior angle of a regular heptagon' do
+    it 'calculates the exterior angle of a regular heptagon' do
       expect(external_angle(7)).to eq(51.43)
     end
 
-    it 'it calculates the exterior angle of a regular octagon' do
+    it 'calculates the exterior angle of a regular octagon' do
       expect(external_angle(8)).to eq(45)
     end
 
-    it 'it calculates the exterior angle of a regular nonagon' do
+    it 'calculates the exterior angle of a regular nonagon' do
       expect(external_angle(9)).to eq(40)
     end
 
-    it 'it calculates the exterior angle of a regular decagon' do
+    it 'calculates the exterior angle of a regular decagon' do
       expect(external_angle(10)).to eq(36)
     end
   end
