0b7f86423b75a7e79a6e73f63cafa8b5cbe29d2c
Luke Hill <lukehill_uk@hotmail.com>
2020-04-22 16:24:51 +0200

    Split up proxy spec a bit more

--- proxy/spec/proxy_spec.rb
+++ proxy/spec/proxy_spec.rb
@@ -1,82 +1,92 @@
 # frozen_string_literal: true
 
-# Project: Create a Proxy Class
-#
-# In this assignment, create a proxy class (one is started for you
-# below).  You should be able to initialize the proxy object with any
-# object.  Any messages sent to the proxy object should be forwarded
-# to the target object.  As each message is sent, the proxy should
-# record the name of the method sent.
-#
-# The proxy class is started for you.  You will need to add a method
-# missing handler and any other supporting methods.
-
 require 'proxy'
 require 'television'
 
 RSpec.describe Proxy do
-  it 'returns a wrapped object' do
-    tv = Proxy.new(Television.new)
+  context 'with a tv proxy' do
+    let(:tv_proxy) { Proxy.new(Television.new) }
 
-    expect(tv).to be_a(Proxy)
+    it 'returns a wrapped object' do
+      expect(tv_proxy).to be_a(Proxy)
+    end
+
+    it 'operates the TV channel button as expected' do
+      tv_proxy.channel = 10
+
+      expect(tv_proxy.channel).to eq(10)
+    end
+
+    it 'operates the TV power button as expected' do
+      tv_proxy.power
+
+      expect(tv_proxy).to be_on
+    end
+
+    it 'records messages sent to the tv' do
+      tv_proxy.power
+      tv_proxy.channel = 10
+
+      expect(tv_proxy.messages).to eq([:power, :channel=])
+    end
+
+    it 'handles invalid messages' do
+      expect { tv_proxy.no_such_method }.to raise_error(NoMethodError)
+    end
+
+    it 'reports proxy methods that have been called' do
+      tv_proxy.power
+
+      expect(tv_proxy.called?(:power)).to be_truthy
+    end
+
+    it 'does not report proxy methods that have not been called' do
+      tv_proxy.channel = 10
+
+      expect(tv_proxy.called?(:power)).to be_falsey
+    end
+
+    it 'can count if a method has not been called' do
+      tv_proxy.power
+
+      expect(tv_proxy.number_of_times_called(:on?)).to eq(0)
+    end
+
+    it 'can count if a method has been called once' do
+      tv_proxy.channel = 48
+
+      expect(tv_proxy.number_of_times_called(:channel=)).to eq(1)
+    end
+
+    it 'can count if a method has been called multiple times' do
+      tv_proxy.power
+      tv_proxy.power
+      tv_proxy.channel = 48
+      tv_proxy.power
+
+      expect(tv_proxy.number_of_times_called(:power)).to eq(3)
+    end
   end
+  
+  context 'with a string proxy' do
+    let(:string_proxy) { Proxy.new('Code Mash 2009') }
 
-  it 'still operates the TV as expected' do
-    tv = Proxy.new(Television.new)
+    it 'returns a wrapped object' do
+      expect(string_proxy).to be_a(Proxy)
+    end
 
-    tv.channel = 10
-    tv.power
+    it 'can perform basic string methods' do
+      string_proxy.upcase!
+      result = string_proxy.split
 
-    expect(tv.channel).to eq(10)
-    expect(tv).to be_on
-  end
+      expect(result).to eq(['CODE', 'MASH', '2009'])
+    end
 
-  it 'records messages sent to the tv' do
-    tv = Proxy.new(Television.new)
+    it 'records messages sent to the string' do
+      string_proxy.upcase
+      string_proxy.split
 
-    tv.power
-    tv.channel = 10
-
-    expect([:power, :channel=]).to eq(tv.messages)
-  end
-
-  it 'handles invalid messages' do
-    tv = Proxy.new(Television.new)
-
-    expect { tv.no_such_method }.to raise_error(NoMethodError)
-  end
-
-  it 'reports proxy methods have been called' do
-    tv = Proxy.new(Television.new)
-
-    tv.power
-    tv.power
-
-    expect(tv.called?(:power)).to be_truthy
-    expect(tv.called?(:channel)).to be_falsey
-  end
-
-  it 'counts method calls' do
-    tv = Proxy.new(Television.new)
-
-    tv.power
-    tv.channel = 48
-    tv.power
-
-    expect(tv.number_of_times_called(:power)).to eq(2)
-    expect(tv.number_of_times_called(:channel=)).to eq(1)
-    expect(tv.number_of_times_called(:on?)).to eq(0)
-  end
-
-  it 'proxies any object' do
-    proxy = Proxy.new('Code Mash 2009')
-
-    proxy.upcase!
-    result = proxy.split
-
-    expect(result).to eq(['CODE', 'MASH', '2009'])
-    expect(proxy.messages).to eq([:upcase!, :split])
+      expect(string_proxy.messages).to eq([:upcase, :split])
+    end
   end
 end
-
-
