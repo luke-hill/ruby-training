5fdbf62bf9c3d9372415857366c866aaee29040e
Malte Gergeleit <malte.gergeleit@outlook.de>
2020-04-23 17:05:00 +0200

    Can sing about lots of cases of bottles

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -2,6 +2,11 @@
 
 class Bottles
   def verse(argument)
-    "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n"
+    case argument
+      when 1
+      "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
+      when 2..99
+      "#{argument} bottles of beer on the wall, #{argument} bottles of beer.\nTake one down and pass it around, #{argument-1} bottles of beer on the wall.\n"
+    end
   end
 end
