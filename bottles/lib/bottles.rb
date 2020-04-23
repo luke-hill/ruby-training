dbdf6932d5f2460ebe94538efaf6409c6d1e1667
Malte Gergeleit <malte.gergeleit@outlook.de>
2020-04-23 20:43:28 +0200

    Bottles almost working

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,24 +1,27 @@
 # frozen_string_literal: true
 
 class Bottles
-  def verse(arg1,arg2= :default)
-    if arg2 == :default
-      arg2 = arg1
-    else
-    end
+  def song
+      verses(99,0)
+  end
 
-    verses = [*arg1..arg2]
-    verses.each do |verse|
-      case verse
-      when 3..99
-        "#{verse} bottles of beer on the wall, #{verse} bottles of beer.\nTake one down and pass it around, #{verse-1} bottles of beer on the wall.\n"
-      when 2
-        "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
-      when 1
-        "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
-      when 0
-        "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
-      end
+  def verses(arg1,arg2)
+    args = [*arg1..arg2]
+    args.each do |arg1|
+      verse(arg1)
     end
+  end
+
+  def verse(arg1)
+      case
+        when arg1 > 2
+          "#{arg1} bottles of beer on the wall, #{arg1} bottles of beer.\nTake one down and pass it around, #{arg1-1} bottles of beer on the wall.\n"
+        when arg1 == 2
+          "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
+        when arg1 == 1
+          "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
+        when arg1 == 0
+          "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
+      end
   end
 end
