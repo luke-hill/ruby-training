f4aa8849b30d7d7419c9f4471a39844560b08041
Malte Gergeleit <malte.gergeleit@outlook.de>
2020-04-23 17:08:22 +0200

    I should propably learn to stash instead of commit

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -3,10 +3,14 @@
 class Bottles
   def verse(argument)
     case argument
-      when 1
-      "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
-      when 2..99
+    when 3..99
       "#{argument} bottles of beer on the wall, #{argument} bottles of beer.\nTake one down and pass it around, #{argument-1} bottles of beer on the wall.\n"
+    when 2
+      "2 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, 1 bottle of beer on the wall.\n"
+    when 1
+      "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
+    when 0
+    "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
     end
   end
 end
