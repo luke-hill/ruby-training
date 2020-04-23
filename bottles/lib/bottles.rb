251211b29de2821998cf953b2b00a42232316c89
Malte Gergeleit <malte.gergeleit@outlook.de>
2020-04-23 17:46:23 +0200

    Stuck on iteration output

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,16 +1,24 @@
 # frozen_string_literal: true
 
 class Bottles
-  def verse(argument)
-    case argument
-    when 3..99
-      "#{argument} bottles of beer on the wall, #{argument} bottles of beer.\nTake one down and pass it around, #{argument-1} bottles of beer on the wall.\n"
-    when 2
-      "2 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, 1 bottle of beer on the wall.\n"
-    when 1
-      "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
-    when 0
-    "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
+  def verse(arg1,arg2= :default)
+    if arg2 == :default
+      arg2 = arg1
+    else
+    end
+
+    verses = [*arg1..arg2]
+    verses.each do |verse|
+      case verse
+      when 3..99
+        "#{verse} bottles of beer on the wall, #{verse} bottles of beer.\nTake one down and pass it around, #{verse-1} bottles of beer on the wall.\n"
+      when 2
+        "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
+      when 1
+        "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
+      when 0
+        "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
+      end
     end
   end
 end
--- bottles/spec/bottles_spec.rb
+++ bottles/spec/bottles_spec.rb
@@ -31,7 +31,6 @@ describe Bottles do
   end
 
   it 'can string a few verses together' do
-    skip
     expected = "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n\n7 bottles of beer on the wall, 7 bottles of beer.\nTake one down and pass it around, 6 bottles of beer on the wall.\n\n6 bottles of beer on the wall, 6 bottles of beer.\nTake one down and pass it around, 5 bottles of beer on the wall.\n\n"
     expect(song.verses(8, 6)).to eq(expected)
   end
