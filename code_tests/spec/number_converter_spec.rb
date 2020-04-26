2119e951b170c1b7fe782b307d25eb7ffe882698
Luke Hill <lukehill_uk@hotmail.com>
2020-04-26 13:06:41 +0200

    Fixed up cops

--- code_tests/spec/number_converter_spec.rb
+++ code_tests/spec/number_converter_spec.rb
@@ -5,83 +5,90 @@ require 'number_converter'
 RSpec.describe NumberConverter do
   it 'can convert 0' do
     question = 0
-    expect('zero').to eq(NumberConverter.new(question).in_english)
+    expect(described_class.new(question).in_english).to eq('zero')
   end
 
   it 'can convert 1' do
     question = 1
-    expect('one').to eq(NumberConverter.new(question).in_english)
+    expect(described_class.new(question).in_english).to eq('one')
   end
 
   it 'can convert 14' do
     question = 14
-    expect('fourteen').to eq(NumberConverter.new(question).in_english)
+    expect(described_class.new(question).in_english).to eq('fourteen')
   end
 
   it 'can convert 20' do
     question = 20
-    expect('twenty').to eq(NumberConverter.new(question).in_english)
+    expect(described_class.new(question).in_english).to eq('twenty')
   end
 
   it 'can convert 22' do
     question = 22
-    expect('twenty-two').to eq(NumberConverter.new(question).in_english)
+    expect(described_class.new(question).in_english).to eq('twenty-two')
   end
 
   it 'can convert 100' do
     question = 100
-    expect('one hundred').to eq(NumberConverter.new(question).in_english)
+    expect(described_class.new(question).in_english).to eq('one hundred')
   end
 
   it 'can convert 123' do
     question = 123
-    expect('one hundred twenty-three').to eq(NumberConverter.new(question).in_english)
+    expect(described_class.new(question).in_english)
+      .to eq('one hundred twenty-three')
   end
 
   it 'can convert 1000' do
     question = 1_000
-    expect('one thousand').to eq(NumberConverter.new(question).in_english)
+    expect(described_class.new(question).in_english).to eq('one thousand')
   end
 
   it 'can convert 1234' do
     question = 1_234
-    expect('one thousand two hundred thirty-four').to eq(NumberConverter.new(question).in_english)
+    expect(described_class.new(question).in_english)
+      .to eq('one thousand two hundred thirty-four')
   end
 
   it 'can convert 1000000' do
     question = 1_000_000
-    expect('one million').to eq(NumberConverter.new(question).in_english)
+    expect(described_class.new(question).in_english).to eq('one million')
   end
 
   it 'can convert 1002345' do
     question = 1_002_345
-    expect('one million two thousand three hundred forty-five').to eq(NumberConverter.new(question).in_english)
+    expect(described_class.new(question).in_english)
+      .to eq('one million two thousand three hundred forty-five')
   end
 
   it 'can convert 1000000000' do
     question = 1_000_000_000
-    expect('one billion').to eq(NumberConverter.new(question).in_english)
+    expect(described_class.new(question).in_english).to eq('one billion')
   end
 
   it 'can convert 987654321123' do
     question = 987_654_321_123
-    expect('nine hundred eighty-seven billion six hundred fifty-four million three hundred twenty-one thousand one hundred twenty-three').to eq(NumberConverter.new(question).in_english)
+    expect(described_class.new(question).in_english)
+      .to eq('nine hundred eighty-seven billion six hundred fifty-four million three hundred twenty-one thousand one hundred twenty-three')
   end
 
   context 'with invalid inputs' do
     it 'does not like inputs that are not numbers' do
       question = 'abc'
-      expect { NumberConverter.new(question).in_english }.to raise_error(ArgumentError)
+      expect { described_class.new(question).in_english }
+        .to raise_error(ArgumentError)
     end
 
     it 'does not like inputs that are not within the lower range' do
       question = -1
-      expect { NumberConverter.new(question).in_english }.to raise_error(ArgumentError)
+      expect { described_class.new(question).in_english }
+        .to raise_error(ArgumentError)
     end
 
     it 'does not like inputs that are not within the upper range' do
       question = 1_000_000_000_000
-      expect { NumberConverter.new(question).in_english }.to raise_error(ArgumentError)
+      expect { described_class.new(question).in_english }
+        .to raise_error(ArgumentError)
     end
   end
 end
