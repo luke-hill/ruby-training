8664ef456f52aedd11265e89ebb9e34d53473f53
Jill <jillannkay@gmail.com>
2020-04-30 19:45:30 +0200

    still working on blocks

--- the-language/spec/2-medium/block_spec.rb
+++ the-language/spec/2-medium/block_spec.rb
@@ -8,14 +8,14 @@ RSpec.describe "ruby's blocks" do
 
   it 'can pass a block to a method' do
     yielded_result = method_with_block { 1 + 2 }
-    expect(yielded_result).to eq(__)
+    expect(yielded_result).to eq(3)
   end
 
   it 'can pass a block using do end' do
     yielded_result = method_with_block do
       1 + 2
     end
-    expect(yielded_result).to eq(__)
+    expect(yielded_result).to eq(3)
   end
 
   def method_with_block_arguments
@@ -24,7 +24,7 @@ RSpec.describe "ruby's blocks" do
 
   it 'can pass an argument to a block' do
     method_with_block_arguments do |argument|
-      expect(argument).to eq(__)
+      expect(argument).to eq('Jim')
     end
   end
 
@@ -38,7 +38,7 @@ RSpec.describe "ruby's blocks" do
   it 'can yield multiple times' do
     result = []
     many_yields { |item| result << item }
-    expect(result).to eq(__)
+    expect(result).to eq([:peanut, :butter, :and, :jelly])
   end
 
   def yield_tester
@@ -50,14 +50,14 @@ RSpec.describe "ruby's blocks" do
   end
 
   it 'can check if a method has been called with a block' do
-    expect(yield_tester { :with_block }).to eq(__)
-    expect(yield_tester).to eq(__)
+    expect(yield_tester { :with_block }).to eq(:with_block)
+    expect(yield_tester).to eq(:no_block)
   end
 
   it 'can affect variables from the code where the block is defined' do
     value = :initial_value
     method_with_block { value = :modified_in_a_block }
-    expect(value).to eq(__)
+    expect(value).to eq(:modified_in_a_block)
   end
 
   it 'can assign a block to variable and be called explicitly' do
