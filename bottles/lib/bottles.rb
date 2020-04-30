5a1f04bfffd0ff572fd17fa9172ada9c273cc6cc
Malte Gergeleit <malte.gergeleit@outlook.de>
2020-04-30 16:55:23 +0200

    No Diff in Bottles but still wrong

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -6,20 +6,36 @@ class Bottles
   end
 
   def verses(arg1, arg2)
-    args = *(arg1..arg2)
-    args.each { |item| verse(item) } # No idea why this doesnt work
+    args = *(arg2..arg1)
+    x = args.reverse.map { |num| verse(num) }.join
+    x << "\n"
   end
 
-  def verse(arg1)
-      case
-        when arg1 > 2
-          "#{arg1} bottles of beer on the wall, #{arg1} bottles of beer.\nTake one down and pass it around, #{arg1-1} bottles of beer on the wall.\n"
-        when arg1 == 2
-          "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
-        when arg1 == 1
-          "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
-        when arg1 == 0
-          "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
-      end
+  def verse(num)
+    "#{line_1(num)}#{line_2(num)}"
+  end
+
+  def line_1(num)
+    case
+      when num > 1
+        "#{num} bottles of beer on the wall, #{num} bottles of beer.\n"
+      when num == 1
+        "1 bottle of beer on the wall, 1 bottle of beer.\n"
+      when num == 0
+        "No more bottles of beer on the wall, no more bottles of beer.\n"
+    end
+  end
+
+  def line_2(num)
+    case
+      when num > 2
+        "Take one down and pass it around, #{num-1} bottles of beer on the wall.\n"
+      when num == 2
+        "Take one down and pass it around, 1 bottle of beer on the wall.\n"
+      when num == 1
+        "Take it down and pass it around, no more bottles of beer on the wall.\n"
+      when num == 0
+        "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+    end
   end
 end
