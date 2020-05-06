2d1746f40f11648310eb4cd93f7e912d7158c98e
Luke Hill <lukehill_uk@hotmail.com>
2020-05-06 14:59:45 +0100

    Add structure for Game tests

--- nim/solution/20_percent/spec/nim/game_spec.rb
+++ nim/solution/20_percent/spec/nim/game_spec.rb
@@ -2,9 +2,38 @@
 
 require 'nim/game'
 
-# Change this test as it basically does nothing!
 RSpec.describe Nim::Game do
-  it 'works' do
-    expect(true).to be_truthy
+  describe '#start' do
+    context 'with no players' do
+      it 'fails to create a game' do
+
+      end
+    end
+
+    context 'with 1 player' do
+      it 'fails to create a game' do
+
+      end
+    end
+
+    context 'with 2 identically named players' do
+      it 'fails to create a game' do
+
+      end
+    end
+
+    context 'with 2 different players' do
+      it 'creates a game' do
+
+      end
+
+      it 'assigns Player 1 as the active player' do
+
+      end
+
+      it 'creates the starting Board' do
+
+      end
+    end
   end
 end
