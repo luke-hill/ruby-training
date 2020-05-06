e5b1fdaa30f10a235271ea0995c21285f6082ede
Luke Hill <lukehill_uk@hotmail.com>
2020-05-06 12:48:52 +0100

    Add / modify valid quadrilateral tests

--- shapes/spec/quadrilateral_spec.rb
+++ shapes/spec/quadrilateral_spec.rb
@@ -8,16 +8,20 @@ RSpec.describe 'quadrilateral types' do
     expect(quadrilateral(90, 90, 90, 90)).to eq([:square, :rectangle])
   end
 
-  it 'is parallelogram or rhombus when there are 2 pairs of angles' do
+  it 'is parallelogram or rhombus when there are 2 pairs of equal angles' do
     expect(quadrilateral(95, 95, 85, 85)).to eq([:parallelogram, :rhombus])
     expect(quadrilateral(120, 60, 120, 60)).to eq([:parallelogram, :rhombus])
     expect(quadrilateral(95, 85, 85, 95)).to eq([:parallelogram, :rhombus])
-    expect(quadrilateral(120, 120, 60, 60)).to eq([:parallelogram, :rhombus])
   end
 
   it 'is a quadrilateral when no angles are equal' do
     expect(quadrilateral(90, 80, 70, 120)).to eq([:quadrilateral])
-    expect(quadrilateral(10, 10, 300, 40)).to eq([:quadrilateral])
+    expect(quadrilateral(10, 20, 300, 30)).to eq([:quadrilateral])
     expect(quadrilateral(95, 5, 100, 160)).to eq([:quadrilateral])
+  end
+
+  it 'is a quadrilateral when there is not 2 pairs of equal angles' do
+    expect(quadrilateral(10, 10, 300, 40)).to eq([:quadrilateral])
+    expect(quadrilateral(80, 80, 80, 120)).to eq([:quadrilateral])
   end
 end
