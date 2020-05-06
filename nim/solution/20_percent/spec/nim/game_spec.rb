9321e1b3d7825f9595d518e0916b680b411aa5e8
Luke Hill <lukehill_uk@hotmail.com>
2020-05-06 14:53:10 +0100

    Add missing files

---
+++ nim/solution/20_percent/spec/nim/game_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'nim/game'
+
+# Change this test as it basically does nothing!
+RSpec.describe Nim::Game do
+  it 'works' do
+    expect(true).to be_truthy
+  end
+end
