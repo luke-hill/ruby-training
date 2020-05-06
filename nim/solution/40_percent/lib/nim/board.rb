cdcc64193ddb99e5fecddbb93bac5a3f44f3036a
Luke Hill <lukehill_uk@hotmail.com>
2020-05-06 17:37:57 +0100

    Added row spec and row configuration

--- nim/solution/40_percent/lib/nim/board.rb
+++ nim/solution/40_percent/lib/nim/board.rb
@@ -10,7 +10,7 @@ module Nim
     attr_reader :rows, :active_player
 
     def initialize(number_of_rows = 5, active_player: :player_one)
-      @rows = Array.new(number_of_rows) { Row.new }
+      @rows = Array.new(number_of_rows) { Row.new(5) }
       @active_player = active_player
     end
 
--- nim/solution/40_percent/lib/nim/row.rb
+++ nim/solution/40_percent/lib/nim/row.rb
@@ -2,5 +2,28 @@
 
 module Nim
   class Row
+    attr_accessor :value
+
+    def initialize(value)
+      @value = value
+    end
+
+    def display
+      "#{stick_display}#{padding}#{number_display}"
+    end
+
+    private
+
+    def stick_display
+      "| " * value
+    end
+
+    def padding
+      " " * 20
+    end
+
+    def number_display
+      "(#{value})"
+    end
   end
 end
