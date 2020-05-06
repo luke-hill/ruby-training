f56f1f0a50b9ddd2399fc5f7fa8ace35558f800c
Luke Hill <lukehill_uk@hotmail.com>
2020-05-06 15:09:57 +0100

    Specs complete for game

--- nim/solution/20_percent/spec/nim/game_spec.rb
+++ nim/solution/20_percent/spec/nim/game_spec.rb
@@ -4,35 +4,51 @@ require 'nim/game'
 
 RSpec.describe Nim::Game do
   describe '#start' do
-    context 'with no players' do
-      it 'fails to create a game' do
+    let(:game) { described_class.new }
 
+    context 'with no players' do
+      allow(game).to receive(:determine_player_one_name) { '' }
+      allow(game).to receive(:determine_player_two_name) { '' }
+
+      it 'fails to create a game' do
+        expect { game.start }.to raise_error(GameSetupError)
       end
     end
 
     context 'with 1 player' do
-      it 'fails to create a game' do
+      allow(game).to receive(:determine_player_one_name) { 'Player One' }
+      allow(game).to receive(:determine_player_two_name) { '' }
 
+      it 'fails to create a game' do
+        expect { game.start }.to raise_error(GameSetupError)
       end
     end
 
     context 'with 2 identically named players' do
-      it 'fails to create a game' do
+      allow(game).to receive(:determine_player_one_name) { 'Player One' }
+      allow(game).to receive(:determine_player_two_name) { 'Player One' }
 
+      it 'fails to create a game' do
+        expect { game.start }.to raise_error(PlayerNameError)
       end
     end
 
     context 'with 2 different players' do
-      it 'creates a game' do
+      allow(game).to receive(:determine_player_one_name) { 'Player One' }
+      allow(game).to receive(:determine_player_two_name) { 'Player Two' }
 
+      it 'creates a game' do
+        expect { game.start }.not_to raise_error
       end
 
       it 'assigns Player 1 as the active player' do
-
+        expect { game.start }.to change(:active_player).to(:player_one)
       end
 
       it 'creates the starting Board' do
+        game.start
 
+        expect(game.board).to be_a(Nim::Board)
       end
     end
   end
