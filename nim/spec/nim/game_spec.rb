8078f53da07509679726549f84a80db9583cfb6a
Luke Hill <lukehill_uk@hotmail.com>
2020-05-06 14:30:12 +0100

    Fix rubocop

--- nim/spec/nim/game_spec.rb
+++ nim/spec/nim/game_spec.rb
@@ -5,6 +5,6 @@ require 'nim/game'
 # Change this test as it basically does nothing!
 RSpec.describe Nim::Game do
   it 'works' do
-    expect(true).to be true
+    expect(true).to be_truthy
   end
 end
