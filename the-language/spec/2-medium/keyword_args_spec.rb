0d8a029fd14bd4b2d5ddf96933286d24fd69396d
Malte Gergeleit <malte.gergeleit@outlook.de>
2020-05-07 12:50:16 +0200

    Done up till message passing

--- the-language/spec/2-medium/keyword_args_spec.rb
+++ the-language/spec/2-medium/keyword_args_spec.rb
@@ -6,10 +6,10 @@ RSpec.describe "ruby's keyword args" do
   end
 
   it 'is possible to define a method with keyword arguments' do
-    expect(method_with_keyword_arguments.class).to eq(__)
-    expect(method_with_keyword_arguments).to eq(__)
-    expect(method_with_keyword_arguments(one: 'one')).to eq(__)
-    expect(method_with_keyword_arguments(two: 2)).to eq(__)
+    expect(method_with_keyword_arguments.class).to eq(Array)
+    expect(method_with_keyword_arguments).to eq([1,'two'])
+    expect(method_with_keyword_arguments(one: 'one')).to eq(['one','two'])
+    expect(method_with_keyword_arguments(two: 2)).to eq([1,2])
   end
 
   def method_with_keyword_arguments_including_mandatory(one, two: 2, three: 3)
@@ -18,6 +18,6 @@ RSpec.describe "ruby's keyword args" do
 
   it 'is possible to define a method with regular and keyword arguments' do
     expect { method_with_keyword_arguments_including_mandatory }
-      .to raise_error(__, /__/)
+      .to raise_error(ArgumentError, /wrong number/)
   end
 end
