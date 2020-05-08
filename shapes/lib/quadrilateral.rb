627322ee7a0a916920afb9026b316af1ffe54193
Malte Gergeleit <malte.gergeleit@outlook.de>
2020-05-08 17:35:10 +0200

    Triangles missing the last Exception, Quads in progress

--- shapes/lib/quadrilateral.rb
+++ shapes/lib/quadrilateral.rb
@@ -1,7 +1,16 @@
 # frozen_string_literal: true
 
 def quadrilateral(a, b, c, d)
-  # WRITE THIS CODE
+  case
+    when  a == 90 && b == 90 && c == 90
+      [:square, :rectangle]
+    when (a == b && c == d) || (a == c && b == d) || (a == d && b == c)
+      [:parallelogram, :rhombus]
+    when a != b && b != c && c != d && d != a
+      [:quadrilateral]
+    when
+      [:quadrilateral]
+  end
 end
 
 # Error class used in invalid_quadrilateral_spec. No need to change this code.
