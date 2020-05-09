f6a197087ef9eb6070cefec8a5548598692f9d2e
Malte Gergeleit <malte.gergeleit@outlook.de>
2020-05-09 14:55:47 +0200

    Quads Working

--- shapes/lib/quadrilateral.rb
+++ shapes/lib/quadrilateral.rb
@@ -2,13 +2,22 @@
 
 def quadrilateral(a, b, c, d)
   case
+    when [a,b,c,d].select { |deg| deg <= 0}.size > 0
+      raise QuadrilateralError
+
+    when [a,b,c,d].sum != 360
+      raise QuadrilateralError
+
     when  a == 90 && b == 90 && c == 90
       [:square, :rectangle]
+
     when (a == b && c == d) || (a == c && b == d) || (a == d && b == c)
       [:parallelogram, :rhombus]
+
     when a != b && b != c && c != d && d != a
       [:quadrilateral]
-    when
+
+    when (a == (b||c||d)) || (b == (c||d)) || (c == d)
       [:quadrilateral]
   end
 end
--- shapes/lib/triangle.rb
+++ shapes/lib/triangle.rb
@@ -4,12 +4,16 @@ def triangle(a, b, c)
   case
     when [a,b,c].select { |side| side <= 0}.size > 0
       raise TriangleError
+
     when a + b <= c || b + c <= a || a + c <= b
       raise TriangleError
+
     when  a == b && b == c
       :equilateral
+
     when (a == b && c != b) || (a == c && b != c) || (b == c && a != c)
       :isosceles
+      
     when  a != b && b != c && c != a
       :scalene
   end
