47bfed7e90c9bbfec2a667dd3556802edfe582e2
Luke Hill <lukehill_uk@hotmail.com>
2021-09-07 15:15:22 +0100

    Add extra gems and some sample pages

---
+++ site_prism/basic_commands/pages/inputs.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class Dropdown < SitePrism::Page
+  set_url 'https://the-internet.herokuapp.com/inputs'
+
+  element :title, 'h3'
+  element :dropdown_list, '#dropdown'
+  elements :dropdown_options, '#dropdown option'
+end
