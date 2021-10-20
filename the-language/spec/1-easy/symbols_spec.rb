0af5affcb9137e62301658367ef163180a78289c
Luke Hill <lukehill_uk@hotmail.com>
2021-10-20 16:55:05 +0100

    Improve symbols spec, add new koan

--- the-language/spec/1-easy/symbols_spec.rb
+++ the-language/spec/1-easy/symbols_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe 'symbols' do
     expect(a_symbol == another_symbol).to eq(__)
   end
 
-  it 'is a single, internal object' do
+  it 'is a single, non-unique internal object' do
     a_symbol        = :a_symbol
     the_same_symbol = :a_symbol
 
@@ -43,7 +43,15 @@ RSpec.describe 'symbols' do
   it 'can be created with spaces' do
     symbol = :"hello world"
 
-    expect(symbol).to eq(__)
+    expect(symbol.is_a?(Symbol)).to eq(__)
+  end
+
+  it 'can be created with an underscore or a hyphen' do
+    underscore_symbol = :"hello_world"
+    hyphenated_symbol = :"hello_world"
+
+    expect(underscore_symbol.is_a?(Symbol)).to eq(__)
+    expect(hyphenated_symbol.is_a?(Symbol)).to eq(__)
   end
 
   it 'can be created with interpolated values' do
