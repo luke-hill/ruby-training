97ff6f35444f4b5c62bc2bbf3f32dd912ecf8b89
max_sukh <maksym.sukharskyi@globacap.com>
2021-10-26 17:44:09 +0300

    true_or_false exercise completion

--- the-language/spec/1-easy/true_false_spec.rb
+++ the-language/spec/1-easy/true_false_spec.rb
@@ -8,24 +8,24 @@ RSpec.describe 'true/truthy and false/falsy' do
   end
 
   it 'treats true as truthy' do
-    expect(truth_tester(true)).to eq(__)
+    expect(truth_tester(true)).to eq(:true_stuff)
   end
 
   it 'treats false as falsey' do
-    expect(truth_tester(false)).to eq(__)
+    expect(truth_tester(false)).to eq(:false_stuff)
   end
 
   it 'treats nil as falsey' do
-    expect(truth_tester(nil)).to eq(__)
+    expect(truth_tester(nil)).to eq(:false_stuff)
   end
 
   it 'treats everything else as truthy' do
-    expect(truth_tester(1)).to eq(__)
-    expect(truth_tester(0)).to eq(__)
-    expect(truth_tester([])).to eq(__)
-    expect(truth_tester({})).to eq(__)
-    expect(truth_tester('Strings')).to eq(__)
-    expect(truth_tester('')).to eq(__)
+    expect(truth_tester(1)).to eq(:true_stuff)
+    expect(truth_tester(0)).to eq(:true_stuff)
+    expect(truth_tester([])).to eq(:true_stuff)
+    expect(truth_tester({})).to eq(:true_stuff)
+    expect(truth_tester('Strings')).to eq(:true_stuff)
+    expect(truth_tester('')).to eq(:true_stuff)
   end
 
   # These are useful, if awkwardly named matchers
@@ -48,8 +48,8 @@ RSpec.describe 'true/truthy and false/falsy' do
     end
 
     it 'can match true exactly with be(true) or eq(true) matchers' do
-      expect(true).to be(__)
-      expect(true).to eq(__)
+      expect(true).to be(true)
+      expect(true).to eq(true)
     end
 
     it 'can match false exactly with be(false) or eq(false) matchers' do
