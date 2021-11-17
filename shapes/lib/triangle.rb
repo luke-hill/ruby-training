d1e41f60741738d18550a57801f7477e81a9fa95
nazarrachunn <87658481+nazarrachunn@users.noreply.github.com>
2021-11-18 00:26:17 +0200

    equilateral_triangle -_- (#30)
    Co-authored-by: nazarrachunn <nazarrachun@gmail.com>

--- shapes/lib/triangle.rb
+++ shapes/lib/triangle.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  equilateral = :equilateral
 end
+
+
 
 # Error class used in invalid_triangle_spec. No need to change this code.
 class TriangleError < StandardError
