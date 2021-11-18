1a45c206ce57ed78f1c81357e9ba6fbba63d70bc
nazarrachunn <87658481+nazarrachunn@users.noreply.github.com>
2021-11-18 23:52:34 +0200

    Quadrilateral exercise (#32)
    * equilateral_triangle -_-
    
    * morning
    
    * seems it is correct
    
    * next step
    
    * Migrate shapes specs into single file as per standards
    
    * Slight tweaks to dice games
    
    Co-authored-by: nazarrachunn <nazarrachun@gmail.com>
    Co-authored-by: Luke Hill <lukehill_uk@hotmail.com>

--- shapes/lib/quadrilateral.rb
+++ shapes/lib/quadrilateral.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 def quadrilateral(a, b, c, d)
-  # WRITE THIS CODE
-end
+  sum = 360
+  return [:square, :rectangle] if a == b && b == c && c == d
+  return [:parallelogram, :rhombus] if a+b != c+d or a+d != b+d or a+d != b+c
 
+end
 # Error class used in invalid_quadrilateral_spec. No need to change this code.
 class QuadrilateralError < StandardError
 end
--- shapes/lib/triangle.rb
+++ shapes/lib/triangle.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 def triangle(a, b, c)
-  return :equilateral if a == b && a == c 
+
+  return :equilateral if a == b && a == c
   return :isosceles if a == b || b == c || a == c
   return :scalene if a != b && b != c && a != c
 end
