39af07e5897f484b289e188d7add4d13e77649d5
nazarrachunn <87658481+nazarrachunn@users.noreply.github.com>
2021-11-18 11:16:26 +0200

    Triange exercise (#31)
    * equilateral_triangle -_-
    
    * morning
    
    * seems it is correct
    
    Co-authored-by: nazarrachunn <nazarrachun@gmail.com>

--- shapes/lib/triangle.rb
+++ shapes/lib/triangle.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 def triangle(a, b, c)
-  equilateral = :equilateral
+  return :equilateral if a == b && a == c 
+  return :isosceles if a == b || b == c || a == c
+  return :scalene if a != b && b != c && a != c
 end
-
-
 
 # Error class used in invalid_triangle_spec. No need to change this code.
 class TriangleError < StandardError
