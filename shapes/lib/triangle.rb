607fdb21cfac351accf3ae1e1a01855dc3f1c058
Bargavi19 <bargavi.muthusubramanian@globacap.com>
2021-11-18 20:27:43 +0000

    triangle_2nd_branch_commits

--- shapes/lib/triangle.rb
+++ shapes/lib/triangle.rb
@@ -1,9 +1,18 @@
 # frozen_string_literal: true
 
 def triangle(a, b, c)
+  # Below code will check errors for
+  # triangles with side length 0
+  # triangles with a negative side length
+  # illegal triangles that are not constructable
   sides = [a, b, c].sort
-  if sides.first == 0 || sides[0] + sides[1] <= sides[2]
+  if sides.first <= 0 || sides[0] + sides[1] <= sides[2]
     raise TriangleError
+  end
+  case [a, b, c].uniq.size
+  when 1 then :equilateral
+  when 2 then :isosceles
+  else        :scalene
   end
 end
 
