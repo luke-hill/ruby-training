aff509b11c25bd07d2e3fbbc65af8e2fd7dd02f9
Bargavi19 <88387622+Bargavi19@users.noreply.github.com>
2021-11-18 15:01:39 +0000

    invalid_triangle_spec_commits (#29)

--- shapes/lib/triangle.rb
+++ shapes/lib/triangle.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  sides = [a, b, c].sort
+  if sides.first == 0 || sides[0] + sides[1] <= sides[2]
+    raise TriangleError
+  end
 end
 
 # Error class used in invalid_triangle_spec. No need to change this code.
