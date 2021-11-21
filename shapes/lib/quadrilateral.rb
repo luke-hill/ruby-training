4569810e3c2d7e8085d7e43a4e798127ec844a7f
nazarrachunn <nazarrachun@gmail.com>
2021-11-21 23:32:29 +0200

    first error

--- shapes/lib/quadrilateral.rb
+++ shapes/lib/quadrilateral.rb
@@ -4,7 +4,8 @@ def quadrilateral(a, b, c, d)
   sum = 360
   return [:square, :rectangle] if a == b && b == c && c == d
   return [:parallelogram, :rhombus] if a+b != c+d or a+d != b+d or a+d != b+c
-
+  return [:parallelogram, :rhombus] if ((a==b) && (c==d)) || ((a==d) && (b==d)) || ((a==d) && (b==c))
+  return [:quadrilateral]
 end
 # Error class used in invalid_quadrilateral_spec. No need to change this code.
 class QuadrilateralError < StandardError
--- shapes/lib/triangle.rb
+++ shapes/lib/triangle.rb
@@ -1,12 +1,25 @@
 # frozen_string_literal: true
 
 def triangle(a, b, c)
+  if (a == 0 || b == 0 || c == 0)
+    raise TriangleError
+  end
+  
+  if (a == b) && (a == c)
+    return :equilateral 
+  end
 
-  return :equilateral if a == b && a == c
-  return :isosceles if a == b || b == c || a == c
-  return :scalene if a != b && b != c && a != c
+  if (a == b) || (b == c) || (a == c)
+    return :isosceles
+  end
+
+  if (a != b) && (b != c) && (a != c)
+    return :scalene
+  end
+
+  
 end
 
-# Error class used in invalid_triangle_spec. No need to change this code.
+# Error class used in invald_triangle_spec. No need to change this code.
 class TriangleError < StandardError
 end
