718bbe822d7842e9e2bbebf52160a8a7e0b0637c
nazarrachunn <87658481+nazarrachunn@users.noreply.github.com>
2021-11-23 11:27:46 +0200

    Quadrilateral exercise (#33)
    * equilateral_triangle -_-
    
    * morning
    
    * seems it is correct
    
    * next step
    
    * quadrilateral fix
    
    * update form upstream
    
    Co-authored-by: nazarrachunn <nazarrachun@gmail.com>

--- shapes/lib/triangle.rb
+++ shapes/lib/triangle.rb
@@ -1,30 +1,9 @@
 # frozen_string_literal: true
 
 def triangle(a, b, c)
-
-  if (a + b <= c || a + c <= b || b + c <= a)
-    raise TriangleError
-  end
-
-  if (a < 0 || b < 0 || c < 0 )
-    raise TriangleError
-  end
-
-  if (a == 0 || b == 0 || c == 0)
-    raise TriangleError
-  end
-
-  if (a == b) && (a == c)
-    return :equilateral
-  end
-
-  if (a == b) || (b == c) || (a == c)
-    return :isosceles
-  end
-
-  if (a != b) && (b != c) && (a != c)
-    return :scalene
-  end
+  return :equilateral if a == b && a == c
+  return :isosceles if a == b || b == c || a == c
+  return :scalene if a != b && b != c && a != c
 end
 
 # Error class used in invald_triangle_spec. No need to change this code.
