a58ccd126f74c5381c51ed31eea384a86302c52d
nazarrachunn <87658481+nazarrachunn@users.noreply.github.com>
2021-11-23 16:43:57 +0200

    Polygon new (#37)
    * resolving
    
    * finally
    
    * next step
    
    Co-authored-by: nazarrachunn <nazarrachun@gmail.com>

--- shapes/lib/polygon_angles.rb
+++ shapes/lib/polygon_angles.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 def internal_angle(number_of_sides)
-  pentagon_sum = 180*(number_of_sides - 2)
-  pentagon_sum / number_of_sides
+  sum = (((number_of_sides - 2)*180) / number_of_sides.to_f).round(2)
+  #result = sum / number_of_sides
+
 end
 
 def external_angle(number_of_sides)
-  # WRITE THIS CODE
+  
 end
