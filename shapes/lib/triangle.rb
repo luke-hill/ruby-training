0c2b4fd1d74ddeeda5b56614cc42deff70357e60
nazarrachunn <87658481+nazarrachunn@users.noreply.github.com>
2021-11-23 11:54:11 +0200

    Quadrilateral (#35)
    * long day today
    
    * last BUG
    
    * seems like this is bad way
    
    * pentagon start
    
    Co-authored-by: nazarrachunn <nazarrachun@gmail.com>

--- shapes/lib/triangle.rb
+++ shapes/lib/triangle.rb
@@ -1,9 +1,31 @@
 # frozen_string_literal: true
 
 def triangle(a, b, c)
-  return :equilateral if a == b && a == c
-  return :isosceles if a == b || b == c || a == c
-  return :scalene if a != b && b != c && a != c
+
+  if (a + b <= c || a + c <= b || b + c <= a)
+    raise TriangleError
+  end
+
+  if (a < 0 || b < 0 || c < 0 )
+    raise TriangleError
+  end
+
+  if (a == 0 || b == 0 || c == 0)
+    raise TriangleError
+  end
+
+  if (a == b) && (a == c)
+    return :equilateral
+  end
+
+  if (a == b) || (b == c) || (a == c)
+    return :isosceles
+  end
+
+  if (a != b) && (b != c) && (a != c)
+    return :scalene
+  end
+
 end
 
 # Error class used in invald_triangle_spec. No need to change this code.
