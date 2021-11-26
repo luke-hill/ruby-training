242f90195e47079e2e2236be3f1e9e9478aa7acb
Luke Hill <lukehill_uk@hotmail.com>
2021-11-26 09:27:22 +0000

    Refactor 4)ii) Use attr reader

--- dice_games/lib/dice.rb
+++ dice_games/lib/dice.rb
@@ -1,13 +1,11 @@
 # frozen_string_literal: true
 
 class Dice
+  attr_reader :values
+
   def roll(amount = 1)
     @values = []
     amount.times { |i| @values << i }
-    @values
-  end
-
-  def values
     @values
   end
 end
