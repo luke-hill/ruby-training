a5199ca8b04cb0741ee12417a33c5381143a01f8
nazarrachunn <nazarrachun@gmail.com>
2021-11-28 18:19:37 +0200

    end

--- shapes/lib/polygon_angles.rb
+++ shapes/lib/polygon_angles.rb
@@ -3,7 +3,6 @@
 def internal_angle(number_of_sides)
   sum = (((number_of_sides - 2)*180) / number_of_sides.to_f).round(2)
   #result = sum / number_of_sides
-
 end
 
 def external_angle(number_of_sides)
