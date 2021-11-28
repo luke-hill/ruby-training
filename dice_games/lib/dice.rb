5fb9d798650c8c30a8cd25e3467325bbdec42f6f
adminn <sridhar.patil@globacap.com>
2021-11-28 19:09:07 +0530

    Updated code(red-green-refactor)

--- dice_games/lib/dice.rb
+++ dice_games/lib/dice.rb
@@ -2,15 +2,19 @@
 
 class Dice
 
-      def roll(n)
+      def roll(n=1)
         @values = []
           i=0
+          if (n==1)
+            @values[i] = rand(1..6)
+          else
           while(i<=(n-1))
             @values[i]=rand(1..5)
             i +=1
           end
           @values
           end
+        end
       def values
         @values
         end
