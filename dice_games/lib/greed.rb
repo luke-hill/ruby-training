8928358f22c7d72e4490565e76d28963e19c44dc
Luke Hill <lukehill_uk@hotmail.com>
2021-11-29 11:32:55 +0000

    Add initial long-winded way of scoring single digits

--- dice_games/lib/greed.rb
+++ dice_games/lib/greed.rb
@@ -3,12 +3,32 @@
 class Greed
   def score(array = nil)
     raise GreedError unless array
+
+    if array == []
+      0
+    elsif array == [5]
+      50
+    elsif array == [1]
+      100
+    else
+      0
+    end
   end
 end
 
 class GreedOther
   def score(array = nil)
     raise GreedError unless array
+    
+    if array == []
+      0
+    elsif array == [5]
+      50
+    elsif array == [1]
+      100
+    else
+      0
+    end
   end
 end
 
