cc9904f6aa57f9de2308a2b04c766d691c6a5b81
Luke Hill <lukehill_uk@hotmail.com>
2021-11-29 11:49:19 +0000

    Implement rules for scoring triples

--- dice_games/lib/greed.rb
+++ dice_games/lib/greed.rb
@@ -8,13 +8,11 @@ class Greed
   def score(array = nil)
     raise GreedError unless array
 
-    array.each do |value|
-      if value == 5
-        @cumulative_score += single_score(5)
-      elsif value == 1
-        @cumulative_score += single_score(1)
+    array.tally.each do |value, quantity|
+      if quantity == 3
+        @cumulative_score += triple_score(value)
       else
-        @cumulative_score += 0
+        @cumulative_score += single_score(value)
       end
     end
 
@@ -22,6 +20,14 @@ class Greed
   end
 
   private
+
+  def triple_score(digit)
+    if digit == 1
+      1000
+    else
+      100 * digit
+    end
+  end
 
   def single_score(digit)
     if digit == 1
@@ -43,38 +49,35 @@ class GreedOther
     raise GreedError unless array
 
     array.each do |value|
-      if value == 5
-        @cumulative_score += five_score
-      elsif value == 1
-        @cumulative_score += one_score
-      else
-        @cumulative_score += other_score
-      end
+      @cumulative_score += score_amount(value, quantity)
     end
 
-    if array == []
-      @cumulative_score += other_score
-    elsif array == [5]
-      @cumulative_score += five_score
-    elsif array == [1]
-      @cumulative_score += one_score
-    else
-      @cumulative_score += other_score
-    end
     @cumulative_score
   end
 
   private
 
-  def one_score
-    100
+  def score_amount(digit, quantity)
+    return one_score(quantity) if digit == 1
+    return five_score(quantity) if digit == 5
+    other_score(digit)
   end
 
-  def five_score
-    50
+  def one_score(quantity)
+    return 1000 if quantity == 3
+
+    100 * quantity
   end
 
-  def other_score
+  def five_score(quantity)
+    return 500 if quantity == 3
+
+    50 * quantity
+  end
+
+  def other_score(value, quantity)
+    return 100 * value if quantity == 3
+
     0
   end
 end
