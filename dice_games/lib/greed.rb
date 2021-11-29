f1f0ec91351060c98e5fba08d1246a5d874d724d
Luke Hill <lukehill_uk@hotmail.com>
2021-11-29 11:31:03 +0000

    Initial solution for first spec

--- dice_games/lib/greed.rb
+++ dice_games/lib/greed.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 class Greed
+  def score(array = nil)
+    raise GreedError unless array
+  end
 end
+
+class GreedOther
+  def score(array = nil)
+    raise GreedError unless array
+  end
+end
+
+GreedError = Class.new(ArgumentError)
