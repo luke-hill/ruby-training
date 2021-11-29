8f98a157a7d00d5b97684865e19586732a457a33
Luke Hill <lukehill_uk@hotmail.com>
2021-11-29 12:09:16 +0000

    Added DRYer form of top method to showcase how analysing something can reduce code

--- dice_games/lib/greed.rb
+++ dice_games/lib/greed.rb
@@ -76,4 +76,36 @@ class GreedOther
   end
 end
 
+class GreedDry
+  def score(array = nil)
+    raise GreedError unless array
+
+    array.tally.map { |value, quantity| bonus_score(value, quantity) + default_score(value, quantity) }.sum
+  end
+
+  private
+
+  def bonus_score(digit, quantity)
+    if quantity < 3
+      0
+    elsif digit == 1
+      700
+    elsif digit == 5
+      350
+    else
+      100 * digit
+    end
+  end
+
+  def default_score(digit, quantity)
+    if digit == 1
+      100 * quantity
+    elsif digit == 5
+      50 * quantity
+    else
+      0
+    end
+  end
+end
+
 GreedError = Class.new(ArgumentError)
