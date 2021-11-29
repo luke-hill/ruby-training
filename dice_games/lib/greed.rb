791bbac002f25abe6a49887b4ff03d39b3509c60
Luke Hill <lukehill_uk@hotmail.com>
2021-11-29 11:41:31 +0000

    Further refactor and enumeration

--- dice_games/lib/greed.rb
+++ dice_games/lib/greed.rb
@@ -1,20 +1,26 @@
 # frozen_string_literal: true
 
 class Greed
+  def initialize
+    @cumulative_score = 0
+  end
+
   def score(array = nil)
     raise GreedError unless array
 
-    if array == []
-      0
-    elsif array == [5]
-      single_score(5)
-    elsif array == [1]
-      single_score(1)
-    else
-      0
+    array.each do |value|
+      if value == 5
+        @cumulative_score += single_score(5)
+      elsif value == 1
+        @cumulative_score += single_score(1)
+      else
+        @cumulative_score += 0
+      end
     end
+
+    @cumulative_score
   end
-  
+
   private
 
   def single_score(digit)
@@ -29,18 +35,33 @@ class Greed
 end
 
 class GreedOther
+  def initialize
+    @cumulative_score = 0
+  end
+
   def score(array = nil)
     raise GreedError unless array
 
-    if array == []
-      other_score
-    elsif array == [5]
-      five_score
-    elsif array == [1]
-      one_score
-    else
-      other_score
+    array.each do |value|
+      if value == 5
+        @cumulative_score += five_score
+      elsif value == 1
+        @cumulative_score += one_score
+      else
+        @cumulative_score += other_score
+      end
     end
+
+    if array == []
+      @cumulative_score += other_score
+    elsif array == [5]
+      @cumulative_score += five_score
+    elsif array == [1]
+      @cumulative_score += one_score
+    else
+      @cumulative_score += other_score
+    end
+    @cumulative_score
   end
 
   private
