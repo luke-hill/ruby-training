f9bcda023dd55d4a7fe7b8e13023767b07306611
Luke Hill <lukehill_uk@hotmail.com>
2021-12-03 14:58:39 +0000

    Refactor 2 - Remove all conditional logic

--- project_answers/bottles.rb
+++ project_answers/bottles.rb
@@ -2,10 +2,6 @@
 
 class Bottles
   def verse(number)
-    if number == 3
-      "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number - 1} bottles of beer on the wall.\n"
-    else
-      "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number - 1} bottles of beer on the wall.\n"
-    end
+    "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number - 1} bottles of beer on the wall.\n"
   end
 end
