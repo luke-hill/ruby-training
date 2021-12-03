063b73f49b6a44e15123335b4d115a99d23a2279
Luke Hill <lukehill_uk@hotmail.com>
2021-12-03 14:58:20 +0000

    Refactor 1 - Make the interpolated number work with the song in all instances

--- project_answers/bottles.rb
+++ project_answers/bottles.rb
@@ -3,9 +3,9 @@
 class Bottles
   def verse(number)
     if number == 3
-      "3 bottles of beer on the wall, 3 bottles of beer.\nTake one down and pass it around, 2 bottles of beer on the wall.\n"
+      "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number - 1} bottles of beer on the wall.\n"
     else
-      "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n"
+      "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number - 1} bottles of beer on the wall.\n"
     end
   end
 end
