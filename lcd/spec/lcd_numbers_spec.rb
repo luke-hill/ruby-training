0770a2b20d6d1c8ffecfb8b046728f8d4474a1b0
Luke Hill <lukehill_uk@hotmail.com>
2021-12-06 15:48:07 +0000

    Improve numbers spec

--- lcd/spec/lcd_numbers_spec.rb
+++ lcd/spec/lcd_numbers_spec.rb
@@ -42,6 +42,7 @@ describe LCD do
   context 'when viewing multiple digits' do
     let(:four_eight_seven) { described_class.new(487) }
     let(:nine_nine_one_three) { described_class.new(9913) }
+    let(:all_digits) { described_class.new(9876543210) }
 
     it 'renders 487' do
       expected = <<~SQUIGGLY_HEREDOC
@@ -61,6 +62,16 @@ describe LCD do
       SQUIGGLY_HEREDOC
 
       expect(nine_nine_one_three.render).to eq(expected)
+    end
+
+    it 'renders all digits' do
+      expected = <<~SQUIGGLY_HEREDOC
+         _  _  _  _  _     _  _     _ 
+        |_||_|  ||_ |_ |_| _| _|  || |
+         _||_|  ||_| _|  | _||_   ||_|
+      SQUIGGLY_HEREDOC
+
+      expect(all_digits.render).to eq(expected)
     end
   end
 end
