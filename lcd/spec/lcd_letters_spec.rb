3f676c76b3948308fd6f84bd51bd5e17ab4498a2
Luke Hill <lukehill_uk@hotmail.com>
2021-12-06 15:44:31 +0000

    Improve letters spec for lcd

--- lcd/spec/lcd_letters_spec.rb
+++ lcd/spec/lcd_letters_spec.rb
@@ -6,6 +6,7 @@ describe LCD do
   context 'when viewing letters from a-f' do
     let(:acb) { described_class.new('acb') }
     let(:fed) { described_class.new('fed') }
+    let(:abcfed) { described_class.new('abcfed') }
 
     it 'renders acb' do
       expected = <<~SQUIGGLY_HEREDOC
@@ -25,6 +26,16 @@ describe LCD do
       SQUIGGLY_HEREDOC
 
       expect(fed.render).to eq(expected)
+    end
+
+    it 'renders all letters' do
+      expected = <<~SQUIGGLY_HEREDOC
+         _        _  _    
+         _||_  _ |_ |_  _| 
+        |_||_||_ |  |_ |_|
+      SQUIGGLY_HEREDOC
+
+      expect(abcfed.render).to eq(expected)
     end
   end
 end
