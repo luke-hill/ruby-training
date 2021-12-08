aa2ad3a5a44d615571dec39ef7e830d69275dcd8
Luke Hill <lukehill_uk@hotmail.com>
2021-12-08 14:22:06 +0000

    Add POM help

--- site_prism/basic_commands/pages/dropdown.rb
+++ site_prism/basic_commands/pages/dropdown.rb
@@ -1,8 +1,16 @@
 # frozen_string_literal: true
 
 class Dropdown < SitePrism::Page
+  # This DSL statement will procedurally write the base url for this page object
+  # It will be used only when the `#load` method is called, or similarly named boolean checks
+  # such as `#when_loaded` or `#loaded?`
   set_url '/dropdown'
 
+  # Here we define our DSL items. the keyword method invocation at the start classifies the
+  # DSL item, and then subsequent parameters define its name / locator e.t.c.
+  #
+  # Where the keyword is `element` we define a "single" item.
+  # Where the keyword is `elements` we define a "collection" of items
   element :title, 'h3'
   element :dropdown_list, '#dropdown'
   elements :dropdown_options, '#dropdown option'
--- site_prism/basic_commands/pages/inputs.rb
+++ site_prism/basic_commands/pages/inputs.rb
@@ -1,8 +1,16 @@
 # frozen_string_literal: true
 
 class Inputs < SitePrism::Page
+  # This DSL statement will procedurally write the base url for this page object
+  # It will be used only when the `#load` method is called, or similarly named boolean checks
+  # such as `#when_loaded` or `#loaded?`
   set_url '/inputs'
 
+  # Here we define our DSL items. the keyword method invocation at the start classifies the
+  # DSL item, and then subsequent parameters define its name / locator e.t.c.
+  #
+  # Where the keyword is `element` we define a "single" item.
+  # Where the keyword is `elements` we define a "collection" of items
   element :title, 'h3'
   element :number_box, '[type="number"]'
 end
