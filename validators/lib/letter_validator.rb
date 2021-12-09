ba1873dfac22b5cf1a5719375ca6822b8bd5082d
Luke Hill <lukehill_uk@hotmail.com>
2021-12-09 12:43:46 +0000

    Initial validators project

---
+++ validators/lib/letter_validator.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class LetterValidator
+  def validate(letter)
+    # WRITE THIS CODE
+  end
+end
+
+# Do not edit this code!
+class InvalidLetterError < ArgumentError; end
---
+++ validators/lib/number_validator.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class NumberValidator
+  def validate(number)
+    # WRITE THIS CODE
+  end
+end
