d58dc6f660b099fdd59b85a21a7cb79b2534717a
Luke Hill <lukehill_uk@hotmail.com>
2021-12-10 09:27:34 +0000

    Add divisible by 5/7/9 tests

--- validators/lib/number_validator.rb
+++ validators/lib/number_validator.rb
@@ -11,6 +11,9 @@ class NumberValidator
     group << :even if number.even?
     group << :odd if number.odd?
     group << :divisible_by_three if number % 3 == 0
+    group << :divisible_by_five if number % 5 == 0
+    group << :divisible_by_seven if number % 7 == 0
+    group << :divisible_by_nine if number % 9 == 0
 
     group
   end
