538e41c551d611cedc44fdc50b2c680671a1964e
Luke Hill <lukehill_uk@hotmail.com>
2021-12-10 09:28:35 +0000

    Optimisation, use #tap to avoid duplication of calls

--- validators/lib/number_validator.rb
+++ validators/lib/number_validator.rb
@@ -8,13 +8,13 @@ class NumberValidator
   end
 
   def validate(number)
-    group << :even if number.even?
-    group << :odd if number.odd?
-    group << :divisible_by_three if number % 3 == 0
-    group << :divisible_by_five if number % 5 == 0
-    group << :divisible_by_seven if number % 7 == 0
-    group << :divisible_by_nine if number % 9 == 0
-
-    group
+    group.tap do |grp|
+      grp << :even if number.even?
+      grp << :odd if number.odd?
+      grp << :divisible_by_three if number % 3 == 0
+      grp << :divisible_by_five if number % 5 == 0
+      grp << :divisible_by_seven if number % 7 == 0
+      grp << :divisible_by_nine if number % 9 == 0
+    end
   end
 end
