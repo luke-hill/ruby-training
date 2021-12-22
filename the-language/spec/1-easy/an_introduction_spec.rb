f3de60d179bc947ee0b9100f8fb21283750d5d57
Isaac <isaac@Isaacs-MacBook-Pro.local>
2021-12-22 13:41:14 +0000

    introduction

--- the-language/spec/1-easy/an_introduction_spec.rb
+++ the-language/spec/1-easy/an_introduction_spec.rb
@@ -1,25 +1,25 @@
 RSpec.describe 'an introduction to ruby' do
   context 'using expectations' do
     it 'checks reality' do
-      expect(true).to be false # it should be true
+      expect(true).to be true # it should be true
     end
 
     it 'verifies our expectations against reality' do
-      expected = __
+      expected = 2
       actual   = 1 + 1
 
       expect(actual).to be expected
     end
 
     it 'has better ways to compare equality' do
-      expected = __
+      expected = 2
       actual   = 1 + 1
 
       expect(actual).to eq expected
     end
 
-    it 'allows you to fill in values' do
-      expect(1 + 1).to eq(__)
+    it 'will ask you to fill in values' do
+      expect(1 + 1).to eq(2)
     end
   end
 end
