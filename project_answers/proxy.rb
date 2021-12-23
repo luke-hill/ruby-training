949c292dcbc76b95746bb599328876991b28fcdb
Luke Hill <lukehill_uk@hotmail.com>
2021-12-23 13:11:37 +0000

    Add initial handlers for method missing and responding to

--- project_answers/proxy.rb
+++ project_answers/proxy.rb
@@ -1,7 +1,17 @@
 # frozen_string_literal: true
 
 class Proxy
+  attr_reader :proxy_object
+
   def initialize(proxy_object)
     @proxy_object = proxy_object
+  end
+
+  def method_missing(method, args, &block)
+
+  end
+
+  def respond_to?(method, args, &block)
+
   end
 end
