79476094294107e36d3e5200cc485ab1d21d6a51
Luke Hill <lukehill_uk@hotmail.com>
2021-12-23 10:17:43 +0000

    Fix up rubocop add warnings

--- metaprogramming/lib/runtime_method_generator.rb
+++ metaprogramming/lib/runtime_method_generator.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
 
+# This class should have methods in it that generate other methods
+# Do not amend the predefined methods
 class RuntimeMethodGenerator
-  # Do not edit this method at all!
   def predefined
     :predefined
+  end
+
+  def return_funky
+    :funky
   end
 end
