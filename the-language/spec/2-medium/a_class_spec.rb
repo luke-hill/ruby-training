f4d3f12161fb04fd3cf7e69ee588e12c9639cb17
aimantirmizi <syedatirmiziaiman@gmail.com>
2022-01-13 15:35:26 +0000

    Finished medium ruby koans for a_class_spec

--- the-language/spec/2-medium/a_class_spec.rb
+++ the-language/spec/2-medium/a_class_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'a ruby class' do
 
   it 'creates new instances of a class using the new method' do
     fido = Dog.new
-    expect(fido.class).to eq(__)
+    expect(fido.class).to eq(Dog)
   end
 
   class Dog2
@@ -15,36 +15,36 @@ RSpec.describe 'a ruby class' do
 
   it 'can set instance variables' do
     fido = Dog2.new
-    expect(fido.instance_variables).to eq(__)
+    expect(fido.instance_variables).to eq([])
 
     fido.set_name('Fido')
-    expect(fido.instance_variables).to eq(__)
+    expect(fido.instance_variables).to eq([:@name])
   end
 
   it 'can not access instance variables by default' do
     fido = Dog2.new
     fido.set_name('Fido')
 
-    expect { fido.name }.to raise_error(__)
+    expect { fido.name }.to raise_error(NoMethodError)
     expect do
       eval 'fido.@name'
       # NOTE: Using eval because the above line is a syntax error.
-    end.to raise_error(__)
+    end.to raise_error(SyntaxError)
   end
 
   it 'can obtain the value of an instance variable using instance_variable_get' do
     fido = Dog2.new
     fido.set_name('Fido')
 
-    expect(fido.instance_variable_get('@name')).to eq(__)
+    expect(fido.instance_variable_get('@name')).to eq("Fido")
   end
 
   it 'can also obtain the value of an instance variable using instance_eval' do
     fido = Dog2.new
     fido.set_name('Fido')
 
-    expect(fido.instance_eval('@name')).to eq(__) # string version
-    expect(fido.instance_eval { @name }).to eq(__) # block version
+    expect(fido.instance_eval('@name')).to eq('Fido') # string version
+    expect(fido.instance_eval { @name }).to eq("Fido") # block version
   end
 
   class Dog3
@@ -61,7 +61,7 @@ RSpec.describe 'a ruby class' do
     fido = Dog3.new
     fido.set_name('Fido')
 
-    expect(fido.name).to eq(__)
+    expect(fido.name).to eq('Fido')
   end
 
   class Dog4
@@ -76,7 +76,7 @@ RSpec.describe 'a ruby class' do
     fido = Dog4.new
     fido.set_name('Fido')
 
-    expect(fido.name).to eq(__)
+    expect(fido.name).to eq('Fido')
   end
 
   class Dog5
@@ -87,7 +87,7 @@ RSpec.describe 'a ruby class' do
     fido = Dog5.new
 
     fido.name = 'Fido'
-    expect(fido.name).to eq(__)
+    expect(fido.name).to eq("Fido")
   end
 
   class Dog6
@@ -100,11 +100,11 @@ RSpec.describe 'a ruby class' do
 
   it 'uses initialize to set up initial values of instance variables' do
     fido = Dog6.new('Fido')
-    expect(fido.name).to eq(__)
+    expect(fido.name).to eq("Fido")
   end
 
   it 'must match args to new with initializer' do
-    expect { Dog6.new }.to raise_error(__)
+    expect { Dog6.new }.to raise_error(ArgumentError)
     # THINK ABOUT IT: Why is this so?
   end
 
@@ -112,7 +112,7 @@ RSpec.describe 'a ruby class' do
     fido = Dog6.new('Fido')
     rover = Dog6.new('Rover')
 
-    expect(rover.name != fido.name).to eq(__)
+    expect(rover.name != fido.name).to eq(true)
   end
 
   class Dog7
@@ -139,34 +139,34 @@ RSpec.describe 'a ruby class' do
     fido = Dog7.new('Fido')
 
     fidos_self = fido.get_self
-    expect(fidos_self).to eq(__)
+    expect(fidos_self).to eq(fido)
   end
 
   it 'provides a string version of the object with to_s' do
     fido = Dog7.new('Fido')
-    expect(fido.to_s).to eq(__)
+    expect(fido.to_s).to eq("Fido")
   end
 
   it 'automatically uses to_s inside string interpolation' do
     fido = Dog7.new('Fido')
-    expect("My dog is #{fido}").to eq(__)
+    expect("My dog is #{fido}").to eq("My dog is Fido")
   end
 
   it 'uses inspect to provide a string description of the object' do
     fido = Dog7.new('Fido')
-    expect(fido.inspect).to eq(__)
+    expect(fido.inspect).to eq("<Dog named 'Fido'>")
   end
 
   it 'has to_s and inspect on all objects' do
     array = [1, 2, 3]
 
-    expect(array.to_s).to eq(__)
-    expect(array.inspect).to eq(__)
+    expect(array.to_s).to eq("[1, 2, 3]")
+    expect(array.inspect).to eq("[1, 2, 3]")
 
-    expect('STRING'.to_s).to eq(__)
-    expect('STRING'.inspect).to eq(__)
+    expect('STRING'.to_s).to eq('STRING')
+    expect('STRING'.inspect).to eq("\"STRING\"")
 
-    expect(Dog.to_s).to eq(__)
-    expect(Dog.inspect).to eq(__)
+    expect(Dog.to_s).to eq('Dog')
+    expect(Dog.inspect).to eq("Dog")
   end
 end
