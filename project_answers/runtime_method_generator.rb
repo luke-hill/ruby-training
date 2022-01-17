0f6188fbd5e8bd5cefc2de738cbccdf723bb32e1
Luke Hill <lukehill_uk@hotmail.com>
2022-01-17 16:00:09 +0000

    Move all code to sample answers directory

--- project_answers/runtime_method_generator.rb
+++ project_answers/runtime_method_generator.rb
@@ -13,4 +13,12 @@ class RuntimeMethodGenerator
   def return_funky
     :funky
   end
+
+  def add_a_method
+    self.class.define_method(:funky_method) { return_funky }
+  end
+
+  def add_custom_method(method_name)
+    self.class.define_method(method_name) { puts "Called new method: #{method_name}" }
+  end
 end
