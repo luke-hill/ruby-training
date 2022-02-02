8e1b4522fdadf485d3184739925ec00860eb6da0
Aiman Syed <12950084+AimanTirmizi@users.noreply.github.com>
2022-02-02 14:02:37 +0000

    Finished hard ruby koans for pass_by_original_reference_spec (#25)

--- the-language/spec/3-hard/pass_by_original_reference_spec.rb
+++ the-language/spec/3-hard/pass_by_original_reference_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'pass by reference vs pass by value' do
     y = x
     x += 2
 
-    expect(y).to eq(__)
+    expect(y).to eq(123)
   end
 
   it 'is possible to pass something which can be seen as pass by reference' do
@@ -12,7 +12,7 @@ RSpec.describe 'pass by reference vs pass by value' do
     y = x
     x.upcase!
 
-    expect(y).to eq(__)
+    expect(y).to eq("STRING")
   end
 
   it 'is also possible to mutate a larger standard object and pass by reference' do
@@ -20,7 +20,7 @@ RSpec.describe 'pass by reference vs pass by value' do
     y = x
     x.map! { |number| number * 2 }
 
-    expect(y).to eq(__)
+    expect(y).to eq([2, 4, 6])
   end
 
   class Initial
@@ -38,7 +38,7 @@ RSpec.describe 'pass by reference vs pass by value' do
     other = Other.new
     initial.x = other.value
 
-    expect(initial.x == other.value).to be(__)
-    expect(initial.x.object_id == other.value.object_id).to be(__)
+    expect(initial.x == other.value).to be(true)
+    expect(initial.x.object_id == other.value.object_id).to be(false)
   end
 end
