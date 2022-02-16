756f5644134f826cad7c3708e06094d9ddd80450
Isaac Mugenyi <ikaliisa@gmail.com>
2022-02-16 15:00:56 +0000

    scalene triangle done

--- shapes/lib/triangle.rb
+++ shapes/lib/triangle.rb
@@ -5,8 +5,12 @@ def triangle(a, b, c)
     :equilateral
   elsif a==b || b==c || a==c
     :isosceles
+  else
+    :scalene
   end
 end
+
+
 
 # Error class used in invalid_triangle_spec. No need to change this code.
 class TriangleError < StandardError
