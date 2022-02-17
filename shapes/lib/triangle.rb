e25a915bfda53237e6b7eee381760e756f3f745c
Isaac Mugenyi <ikaliisa@gmail.com>
2022-02-17 16:01:19 +0000

    invalid triangles with side length 0 done

--- shapes/lib/triangle.rb
+++ shapes/lib/triangle.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 def triangle(a, b, c)
+  raise TriangleError if a==0 || b==0 || c==0
   if a==b && a==c
     :equilateral
   elsif a==b || b==c || a==c
@@ -11,7 +12,7 @@ def triangle(a, b, c)
 end
 
 
-
 # Error class used in invalid_triangle_spec. No need to change this code.
 class TriangleError < StandardError
 end
+
