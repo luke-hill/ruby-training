6f2f17097dde6c834ea538bd4b7864203089c2d6
Isaac Mugenyi <ikaliisa@gmail.com>
2022-02-18 14:34:08 +0000

    added a conditional statement to square and rectangle

--- shapes/lib/quadrilateral.rb
+++ shapes/lib/quadrilateral.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 def quadrilateral(a, b, c, d)
+  if a==90 && b==90 && c==90 && d==90
   [:square, :rectangle]
+  end
 end
+
 
 # Error class used in invalid_quadrilateral_spec. No need to change this code.
 class QuadrilateralError < StandardError
