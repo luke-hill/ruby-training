77b646a41fe7d9416f5e1d70c2e715c582217360
Luke Hill <lukehill_uk@hotmail.com>
2022-02-18 14:43:04 +0000

    Improve notes on quadrilateral spec

--- shapes/lib/quadrilateral.rb
+++ shapes/lib/quadrilateral.rb
@@ -4,6 +4,6 @@ def quadrilateral(a, b, c, d)
   # WRITE THIS CODE
 end
 
-# Error class used in invalid_quadrilateral_spec. No need to change this code.
+# Error class used in quadrilateral_spec. No need to change this code.
 class QuadrilateralError < StandardError
 end
