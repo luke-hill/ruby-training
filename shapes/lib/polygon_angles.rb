d32546f82b47845332922d762013b335ff595467
Isaac Mugenyi <ikaliisa@gmail.com>
2022-02-21 08:48:32 +0000

    regular hexagon done

--- shapes/lib/polygon_angles.rb
+++ shapes/lib/polygon_angles.rb
@@ -4,6 +4,10 @@ def internal_angle(number_of_sides)
    number_of_sides == [5,6,7,8,9,10]
    if number_of_sides == 5
     108
+   elsif number_of_sides == 6
+     120
+   else
+     128.57
   end
 end
 
