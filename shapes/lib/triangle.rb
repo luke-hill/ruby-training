c578bc63b5d76eba20896b85fd65f3b2ecd73f7e
Aiman Syed <12950084+AimanTirmizi@users.noreply.github.com>
2022-02-22 10:44:11 +0000

    Added TDD tests for triangle in Shapes specs (#32)

--- shapes/lib/triangle.rb
+++ shapes/lib/triangle.rb
@@ -1,7 +1,16 @@
 # frozen_string_literal: true
 
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  a, b, c = [a, b, c].sort
+  if a <= 0 || a + b <= c
+    raise TriangleError
+  elsif a == b
+    :equilateral
+  elsif b == c
+    :isosceles
+  else
+    :scalene
+  end
 end
 
 # Error class used in invalid_triangle_spec. No need to change this code.
