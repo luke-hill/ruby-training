36dd086dbeb16eca5308c05712d6f402c6ac582f
Aiman Syed <12950084+AimanTirmizi@users.noreply.github.com>
2022-02-22 10:38:16 +0000

    Added TDD tests for quadrilateral in Shapes specs (#30)

--- shapes/lib/quadrilateral.rb
+++ shapes/lib/quadrilateral.rb
@@ -1,7 +1,18 @@
 # frozen_string_literal: true
 
 def quadrilateral(a, b, c, d)
-  # WRITE THIS CODE
+  a, b, c, d = [a, b, c, d].sort
+  if a <= 0 || a + b + c + d != 360
+    raise QuadrilateralError
+  elsif a == d
+    [:square, :rectangle]
+  elsif a == b && c == d
+    [:parallelogram, :rhombus]
+  elsif a != c && b != d
+    [:quadrilateral]
+  else a != d
+    [:quadrilateral]
+  end
 end
 
 # Error class used in invalid_quadrilateral_spec. No need to change this code.
