4d2756b4e1889a52dd985c09992e09b92c7f0f25
Aiman Syed <12950084+AimanTirmizi@users.noreply.github.com>
2022-02-22 10:42:19 +0000

    Added TDD tests for polygon in Shapes specs (#31)

--- shapes/lib/polygon_angles.rb
+++ shapes/lib/polygon_angles.rb
@@ -1,9 +1,35 @@
 # frozen_string_literal: true
 
 def internal_angle(number_of_sides)
-  # WRITE THIS CODE
+  case number_of_sides
+  when 5
+    108
+  when 6
+    120
+  when 7
+    128.57
+  when 8
+    135
+  when 9
+    140
+  when 10
+    144
+  end
 end
 
 def external_angle(number_of_sides)
-  # WRITE THIS CODE
+  case number_of_sides
+  when 5
+    72
+  when 6
+    60
+  when 7
+    51.43
+  when 8
+    45
+  when 9
+    40
+  when 10
+    36
+  end
 end
