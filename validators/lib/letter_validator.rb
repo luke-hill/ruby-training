b99bc9bc4a66aa4e648a2028c4fb4e3ab19b0865
Isaac Mugenyi <ikaliisa@gmail.com>
2022-03-02 15:56:56 +0000

    specified vowels & consonats using different method

--- validators/lib/letter_validator.rb
+++ validators/lib/letter_validator.rb
@@ -1,17 +1,31 @@
 # frozen_string_literal: true
 
 class LetterValidator
+  attr_reader :alphabet
+
+  def initialize
+    @alphabet = []
+  end
+
   def validate(letter)
-    raise InvalidLetterError if letter.length>1
+    raise InvalidLetterError if letter.length> 1
+    alphabet<<vowels_check(letter)
+  end
+
+  def vowels_check(letter)
     vowels = %w[a e i o u]
     case letter
     when *vowels
-      [:vowel]
+      :vowel
     else
-      [:consonant]
+      :consonant
     end
   end
+
+
 end
+
+
 
 # Do not edit this code!
 class InvalidLetterError < ArgumentError;
