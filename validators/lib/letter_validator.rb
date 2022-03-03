2f5017132c7dd38e0fea4509c5b378d319144f2c
Aiman Syed <12950084+AimanTirmizi@users.noreply.github.com>
2022-03-03 16:46:49 +0000

    Add TDD tests for letter_validator specs (#34)
    * Add test to validate all vowels
    
    * Modified test to validate InvalidLetterError
    
    * Add test to validate all consonants
    
    * Updated test to validate all consonants and vowels
    
    * Add test methods to validate all conditions
    
    * Add attr_reader and initialize instance variable with an empty array
    
    * Update test methods to return symbols instead of an array value
    
    * Add validate_type method to validate 'Vowel or Consonant' letters
    
    * Add validate_half method to validate first_half or second_half of letters
    
    * Add validate_usage method to validate common letters

--- validators/lib/letter_validator.rb
+++ validators/lib/letter_validator.rb
@@ -1,9 +1,34 @@
 # frozen_string_literal: true
 
 class LetterValidator
-  def validate(letter)
-    # WRITE THIS CODE
+
+  attr_reader :values
+
+  def initialize
+    @values = []
   end
+
+  def validate(letter)
+    raise InvalidLetterError if letter.length > 1
+    values << validate_type(letter)
+    values << validate_half(letter)
+    values << validate_usage(letter)
+  end
+
+  def validate_type(letter)
+    return :vowel if letter.match(/[aeiou]/)
+    :consonant
+  end
+
+  def validate_half(letter)
+    return :first_half if letter.match(/[a-m]/)
+    :second_half
+  end
+
+  def validate_usage(letter)
+    return :common_letter if letter.match(/[etaio]/)
+  end
+
 end
 
 # Do not edit this code!
