9555d74a9b8015e69b17eb9aa6ebb3d144fd50d3
Aiman Syed <12950084+AimanTirmizi@users.noreply.github.com>
2022-03-03 17:15:34 +0000

    Add TDD tests for number_validator specs (#35)
    * Add test to validate odd numbers
    
    * Modified test to validate odd & even numbers
    
    * Add test to validate non-integers
    
    * Add private test methods to validate all conditions
    
    * Add attr_reader and initialize instance variable with an empty array and modified test methods to return symbols instead of an array
    
    * Add validate_type method to validate even or odd numbers
    
    * Add method multiples_of_three
    
    * Add method multiples_of_five
    
    * Add method multiples_of_seven
    
    * Add method multiples_of_nine
    
    * Add method validate_multiples to validate multiples of Three, Five, Seven and Nine

--- validators/lib/number_validator.rb
+++ validators/lib/number_validator.rb
@@ -1,7 +1,45 @@
 # frozen_string_literal: true
 
 class NumberValidator
-  def validate(number)
-    # WRITE THIS CODE
+  attr_reader :values
+
+  def initialize
+    @values = []
   end
+
+  def validate(number)
+    raise ArgumentError if number.is_a?(String)
+    values << validate_type(number)
+    validate_multiples(number)
+
+  end
+
+  def validate_multiples(number)
+    values << multiples_of_three(number)
+    values << multiples_of_five(number)
+    values << multiples_of_seven(number)
+    values << multiples_of_nine(number)
+  end
+
+  def validate_type(number)
+    return :even if number % 2 == 0
+    :odd
+  end
+
+  def multiples_of_three(number)
+    return :divisible_by_three if number % 3 == 0
+  end
+
+  def multiples_of_five(number)
+    return :divisible_by_five if number % 5 == 0
+  end
+
+  def multiples_of_seven(number)
+    return :divisible_by_seven if number % 7 == 0
+  end
+
+  def multiples_of_nine(number)
+    return :divisible_by_nine if number % 9 == 0
+  end
+
 end
