6bdab639ca45bb4b89c7d9c4df8f473aaf35f2e8
Isaac Mugenyi <ikaliisa@gmail.com>
2022-03-03 14:49:48 +0000

    even and odd numbers done

--- validators/lib/number_validator.rb
+++ validators/lib/number_validator.rb
@@ -1,12 +1,21 @@
 # frozen_string_literal: true
 
 class NumberValidator
-  def validate(number)
-    if number % 2 != 0
-      [:odd]
-    else
-      [:even]
-    end
+  attr_reader :values
 
+  def initialize
+    @values = []
+  end
+
+  def validate(number)
+    values<<even_odd(number)
+  end
+
+  def even_odd(number)
+    if number % 2 != 0
+      :odd
+    else
+      :even
+    end
   end
 end
