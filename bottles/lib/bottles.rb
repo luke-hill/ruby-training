6d3baf57e9036638b0582e4356eb6fb10be87bd9
Isaac Mugenyi <ikaliisa@gmail.com>
2022-03-07 16:30:29 +0000

    verse 1 done

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -7,6 +7,8 @@ class Bottles
     "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n"
     when 3
       "3 bottles of beer on the wall, 3 bottles of beer.\nTake one down and pass it around, 2 bottles of beer on the wall.\n"
+    when 1
+      "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
     when 2
     "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
     when 0
--- bottles/spec/bottles_spec.rb
+++ bottles/spec/bottles_spec.rb
@@ -17,7 +17,7 @@ describe Bottles do
   end
 
   it 'can sing about 1 bottle' do
-    skip
+    
     expected = "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
     expect(bottles.verse(1)).to eq(expected)
   end
@@ -29,7 +29,7 @@ describe Bottles do
   end
 
   it 'can sing about no more bottles' do
-    
+
     expected = "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
     expect(bottles.verse(0)).to eq(expected)
   end
