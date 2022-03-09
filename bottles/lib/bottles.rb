44d40d0b27c9cdd556e96b25d50b0d5cbe6dc166
Isaac Mugenyi <ikaliisa@gmail.com>
2022-03-09 15:58:41 +0000

    removed verse 3 and 8 duplication

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -9,9 +9,7 @@ class Bottles
       "#{number} bottle of beer on the wall, #{number} bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
     when 2
       "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number-1} bottle of beer on the wall.\n"
-    when 3
-      "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number-1} bottles of beer on the wall.\n"
-    when 8
+    else
       "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number-1} bottles of beer on the wall.\n"
     end
   end
