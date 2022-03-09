67560abff8af56840c8d76b3474c3bc36a0f1da3
Isaac Mugenyi <ikaliisa@gmail.com>
2022-03-09 14:53:09 +0000

    reordered the verses

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -3,16 +3,19 @@
 class Bottles
   def verse(number)
     case number
-    when 8
-    "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n"
-    when 3
-      "3 bottles of beer on the wall, 3 bottles of beer.\nTake one down and pass it around, 2 bottles of beer on the wall.\n"
+    when 0
+      "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
     when 1
       "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
     when 2
-    "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
-    when 0
-      "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
+      "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
+    when 3
+      "3 bottles of beer on the wall, 3 bottles of beer.\nTake one down and pass it around, 2 bottles of beer on the wall.\n"
+    when 8
+      "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n"
     end
   end
 end
+
+
+
--- bottles/spec/bottles_spec.rb
+++ bottles/spec/bottles_spec.rb
@@ -17,7 +17,7 @@ describe Bottles do
   end
 
   it 'can sing about 1 bottle' do
-    
+
     expected = "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
     expect(bottles.verse(1)).to eq(expected)
   end
