0acc04ce952d2827d25543ccf1c17ba9215e4cf8
Aiman Syed <12950084+AimanTirmizi@users.noreply.github.com>
2022-03-14 11:30:20 +0000

    Add TDD for bottles specs (#37)
    * Add method verse(bottle) to validate typical verse
    
    * Remove 'Skip' to validate few verses together
    
    * Add method verses(*bottles)
    
    * Modified optional args method verses(*bottles) to verses(first_verse, second_verse) & rename argument bottles to first_verse in verse
    
    * Add method number_of_bottles(number) to interpolate bottles values
    
    * Remove 'Skip' to validate another typical verse
    
    * Remove string 'bottlesOnWall'
    
    * Modified method number_of_bottles(number)
    
    * Remove 'Skip' to sing about 1 bottle
    
    * Remove 'Skip' to sing about 2 bottles
    
    * Add condition if number of bottles are 2
    
    * Remove 'Skip' can sing about no more bottles
    
    * Remove 'Skip' can sing the whole song
    
    * Add method 'sing' for the whole song
    
    * Updated strings styling in number_of_bottles(number)
    
    * Updated verses(first_verse, last_verse) method to use downto().map Ruby methods instead of hard-core string
    
    * Update method name to verse(number) from number_of_bottles(number) & remove duplicate verse(first_verse) method

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,4 +1,35 @@
 # frozen_string_literal: true
 
 class Bottles
+  def verses(first_verse, last_verse)
+    first_verse.downto(last_verse).map {|number| verse(number)}.join("\n") + "\n"
+  end
+
+  def sing
+    verses(99,0)
+  end
+
+  def verse(number)
+    if number == 0
+      "No more bottles of beer on the wall, " +
+      "no more bottles of beer.\n" +
+      "Go to the store and buy some more, " +
+      "99 bottles of beer on the wall.\n"
+    elsif number == 1
+      "1 bottle of beer on the wall, " +
+      "1 bottle of beer.\n" +
+      "Take it down and pass it around, " +
+      "no more bottles of beer on the wall.\n"
+    elsif number == 2
+      "2 bottles of beer on the wall, " +
+      "2 bottles of beer.\n" +
+      "Take one down and pass it around, " +
+      "1 bottle of beer on the wall.\n"
+    else
+      "#{number} bottles of beer on the wall, " +
+      "#{number} bottles of beer.\n" +
+      "Take one down and pass it around, "+
+      "#{number - 1} bottles of beer on the wall.\n"
+    end
+  end
 end
