e0f63fd7f33014dea5e432935df93cf003864a71
Isaac Mugenyi <ikaliisa@gmail.com>
2022-03-15 17:00:05 +0000

    removed some duplication

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -6,11 +6,18 @@ class Bottles
     when 0
       "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
     when 1
-      "#{number} bottle of beer on the wall, #{number} bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
-    when 2
-      "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number-1} bottle of beer on the wall.\n"
+      "#{number} #{container(number)} of beer on the wall, #{number} #{container(number)} of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
     else
-      "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number-1} bottles of beer on the wall.\n"
+      "#{number} #{container(number)} of beer on the wall, #{number} #{container(number)} of beer.\nTake one down and pass it around, #{number-1} #{container(number-1)} of beer on the wall.\n"
+    end
+  end
+
+  def container(number)
+    case number
+    when 1
+      "bottle"
+    else
+      "bottles"
     end
   end
 end
