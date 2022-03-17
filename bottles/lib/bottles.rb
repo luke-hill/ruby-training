7c43d7fc737714b124f66c6851b2e96cdad89e62
Isaac Mugenyi <ikaliisa@gmail.com>
2022-03-17 09:13:53 +0000

    changed layout

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,15 +1,12 @@
 # frozen_string_literal: true
 
 class Bottles
-  def verse(number)
-    case number
-    when 0
-      "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
-    when 1
-      "#{number} #{container(number)} of beer on the wall, #{number} #{container(number)} of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
-    else
-      "#{number} #{container(number)} of beer on the wall, #{number} #{container(number)} of beer.\nTake one down and pass it around, #{number-1} #{container(number-1)} of beer on the wall.\n"
-    end
+  def sing
+    verses(99,0)
+  end
+
+  def verses(upper,lower)
+    upper.downto(lower).map {|number| verse(number)}.join("\n")+ "\n"
   end
 
   def container(number)
@@ -21,14 +18,16 @@ class Bottles
     end
   end
 
-  def verses(upper,lower)
-    upper.downto(lower).map {|number| verse(number)}.join("\n")+ "\n"
+  def verse(number)
+    case number
+    when 0
+      "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
+    when 1
+      "#{number} #{container(number)} of beer on the wall, #{number} #{container(number)} of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
+    else
+      "#{number} #{container(number)} of beer on the wall, #{number} #{container(number)} of beer.\nTake one down and pass it around, #{number-1} #{container(number-1)} of beer on the wall.\n"
+    end
   end
-
-  def sing
-    verses(99,0)
-  end
-
   end
 
 
