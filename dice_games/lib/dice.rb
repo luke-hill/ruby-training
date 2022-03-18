bbce1ca08e714282003039a627d07efcf1bc0c7f
Isaac Mugenyi <ikaliisa@gmail.com>
2022-03-18 16:00:23 +0000

    Able to return an array

--- dice_games/lib/dice.rb
+++ dice_games/lib/dice.rb
@@ -1,6 +1,17 @@
 # frozen_string_literal: true
 
 class Dice
-  def roll
+  attr_reader :values
+
+  def initialize
+    @values = []
   end
+
+  def values
+    @values
+  end
+
+  def roll(number_of_dice)
+  end
+
 end
