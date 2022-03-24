a221fb1f54604258a0e8d1d8f9050df2b52146d4
Isaac Mugenyi <ikaliisa@gmail.com>
2022-03-24 12:00:32 +0000

    Specified the rules of number one

--- dice_games/lib/greed.rb
+++ dice_games/lib/greed.rb
@@ -2,19 +2,29 @@
 
 class Greed
   def score(dice)
-    score = 0
-    dice.each do |die|
-      if die == 5
-        score += 50
-      elsif die== 1
-        score += 100
-      elsif
-        0
+    score= 0
+    dice.uniq.each do |number|
+      if number == 1
+        if dice.count(1) >= 3
+          score += 1000
+          score += 100 * (dice.count(1) - 3)
+        else
+          score += 100 * dice.count(1)
+        end
       end
+
     end
     score
   end
-end
+  end
+
+
+
+
+
+
+
+
 
 
 
