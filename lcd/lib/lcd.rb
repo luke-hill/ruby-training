88dd178bac2427aaf353439086fcea2b33e99e0f
Isaac Mugenyi <ikaliisa@gmail.com>
2022-04-26 15:45:27 +0100

    Initial set up

--- lcd/lib/lcd.rb
+++ lcd/lib/lcd.rb
@@ -1,5 +1,31 @@
 # frozen_string_literal: true
 
 class LCD
-  # Write your code in here
+  attr_reader :number_or_string
+
+  def initialize(number_or_string)
+    @number_or_string = number_or_string
+  end
+
+  def render
+  end
+  
+
+  def horizontal_segment(type)
+    case type
+    when 1
+      "_"
+    else
+      " "
+    end
+  end
+
+  def vertical_segment(type)
+    case type
+    when 1
+      "|"
+    else
+      " "
+    end
+  end
 end
