180c68a352b12794fb53d7677b7bc091d23793e2
Isaac Mugenyi <ikaliisa@gmail.com>
2022-05-03 09:28:42 +0100

    numbers and letters done

--- lcd/lib/lcd.rb
+++ lcd/lib/lcd.rb
@@ -8,6 +8,8 @@ class LCD
   end
 
   def render
+    display = single_number_or_letter.map { |number_or_letter| display_character(number_or_letter) }
+    display.transpose.map { |grouped_segments| grouped_segments.push("\n")}.join
   end
 
   def single_number_or_letter
@@ -18,7 +20,24 @@ class LCD
     end
   end
 
-  
+  def display_character(individual_digit_or_char)
+    lcd_states = %w[line_one line_two line_three]
+    lcd_states.map.with_index do |line_number, index|
+      if line_number == "line_one"
+        " " + horizontal_segment(lcd_display[individual_digit_or_char][index]) + " "
+      else
+        other_segments(line_number, individual_digit_or_char, index)
+      end
+    end
+  end
+
+  def other_segments(line_number, digit, segment)
+    if line_number == "line_two"
+      vertical_segment(lcd_display[digit][segment]) + horizontal_segment(lcd_display[digit][segment + 1]) + vertical_segment(lcd_display[digit][segment + 2])
+    else
+      vertical_segment(lcd_display[digit][segment + 2]) + horizontal_segment(lcd_display[digit][segment + 3]) + vertical_segment(lcd_display[digit][segment + 4])
+    end
+  end
 
   def horizontal_segment(type)
     case type
@@ -57,5 +76,6 @@ class LCD
       "e" => [1, 1, 1, 0, 1, 1, 0],
       "d" => [0, 0, 1, 1, 1, 1, 1]
     }
+    end
   end
-end
+
