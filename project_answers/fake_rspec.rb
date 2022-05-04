1e3899d2912dc5d1cd67c6417bd5c304e01684fa
Luke Hill <lukehill_uk@hotmail.com>
2022-05-04 14:07:53 +0100

    Completed solution

--- project_answers/fake_rspec.rb
+++ project_answers/fake_rspec.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require 'rainbow'
+require 'rainbow/refinement'
+using Rainbow
 
 class FakeRSpec
   FakeRSpecError = Class.new(StandardError)
@@ -9,20 +11,23 @@ class FakeRSpec
     attr_accessor :results
 
     def run(description)
-      puts "Running #{description} tests"
+      puts "Running #{description} tests".yellow
       self.results = []
       yield
-      puts 'All Tests finished'
+      puts 'All Tests finished'.yellow
       puts "Result-set: #{results}"
     end
 
     def it(description)
-      puts "Running test #{description}"
+      puts "Running test: #{description}".blue
       yield.tap do |result|
-        puts "Result: #{result}"
+        if result
+          puts "#{description}: Congratulations the test passed".green.underline
+        else
+          puts "#{description}: TEST FAILED!".red.italic.bold
+        end
         results << result
       end
-      puts "Test finished"
     end
   end
 end
