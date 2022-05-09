1a65abaae306edde8d83d08045e7b54f0e4c7a79
Isaac Mugenyi <ikaliisa@gmail.com>
2022-05-09 12:48:32 +0100

    Can track each method called that target object can respond to

--- proxy/lib/proxy.rb
+++ proxy/lib/proxy.rb
@@ -5,6 +5,16 @@ class Proxy
   attr_reader :proxy_object
 
   def initialize(proxy_object)
-    @proxy_object = proxy_object
+    @object = proxy_object
+    @recorded_messages = []
   end
+
+  def method_missing(method_name, *args, &block)
+    if @object.respond_to? method_name
+      @recorded_messages.push method_name
+      @object.send method_name, *args
+    else
+      super(method_name, *args, &block)
+    end
+    end
 end
