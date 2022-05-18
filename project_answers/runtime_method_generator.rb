00dbc8d2dc023e8844d0f226946098399d543e28
Luke Hill <lukehill_uk@hotmail.com>
2022-05-18 08:47:02 +0100

    Improve sample answer for metaprogramming

--- project_answers/runtime_method_generator.rb
+++ project_answers/runtime_method_generator.rb
@@ -19,6 +19,29 @@ class RuntimeMethodGenerator
   end
 
   def add_custom_method(method_name)
-    self.class.define_method(method_name) { puts "Called new method: #{method_name}" }
+    self.class.define_method(method_name) do |*runtime_args, &block|
+      puts "Called new method: #{method_name}"
+      puts "Args passed in: #{runtime_args.join(',')}" unless runtime_args.empty?
+      puts "Runtime block defined: #{block.call}" if block
+    end
   end
 end
+
+# A few examples of how this could be called and the outputs are below
+
+# c = RuntimeMethodGenerator.new
+# c.add_custom_method(:foo)
+#
+# c.foo
+# => Called new method: foo
+#
+# c.foo(:arg1_value) { :hi }
+# => Called new method: foo
+# => Args passed in: arg1_value
+# => Runtime block defined: hi
+#
+# c.foo(:arg1, 2, 'three') { p 'do the thing' }
+# => Called new method: foo
+# => Args passed in: arg1,2,three
+# =>"do the thing"
+# => Runtime block defined: do the thing
