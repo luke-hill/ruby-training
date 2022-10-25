4547e74d850e62bffbe65a2a2a3dd5d67c5d8d8b
Luke Hill <lukehill_uk@hotmail.com>
2022-10-25 12:14:29 +0100

    Convert specs to easier format

--- decorators/spec/burger_spec.rb
+++ decorators/spec/burger_spec.rb
@@ -1,13 +1,47 @@
 # frozen_string_literal: true
 
 require 'burger'
+require 'large_burger'
+require 'cheese_burger'
 
 RSpec.describe Burger do
-  subject(:burger) { described_class.new }
+  context 'with a regular burger' do
+    subject(:burger) { 'FIX ME' }
 
-  describe '#cost' do
-    it 'costs £5' do
-      expect(burger.cost).to eq(500)
+    describe '#cost' do
+      it 'costs £5' do
+        expect(burger.cost).to eq(500)
+      end
+    end
+  end
+
+  context 'with added cheese' do
+    subject(:burger) { 'FIXME' }
+
+    describe '#cost' do
+      it 'costs £5.60' do
+        expect(burger.cost).to eq(560)
+      end
+    end
+  end
+
+  context 'with an extra burger' do
+    subject(:burger) { 'FIXME' }
+
+    describe '#cost' do
+      it 'costs £7.50' do
+        expect(burger.cost).to eq(750)
+      end
+    end
+  end
+
+  context 'with an extra burger AND added cheese' do
+    subject(:burger) { 'FIXME' }
+
+    describe '#cost' do
+      it 'costs £8.10' do
+        expect(burger.cost).to eq(810)
+      end
     end
   end
 end
