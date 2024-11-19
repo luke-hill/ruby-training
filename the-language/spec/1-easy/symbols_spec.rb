eff99556548e0bee9419ac24d49d44a83109c4f5
toby3carter <toby.carter@howsy.com>
2024-11-19 16:43:03 +0000

    Completed the 1-easy project

--- the-language/spec/1-easy/symbols_spec.rb
+++ the-language/spec/1-easy/symbols_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe 'Symbols' do
   it 'is a type of symbol class' do
     symbol = :a_symbol
-    expect(symbol.is_a?(Symbol)).to eq(__)
+    expect(symbol.is_a?(Symbol)).to eq(true)
   end
 
   it 'can compare with other symbols' do
@@ -9,42 +9,42 @@ RSpec.describe 'Symbols' do
     the_same_symbol = :a_symbol
     another_symbol  = :another_symbol
 
-    expect(a_symbol == the_same_symbol).to eq(__)
-    expect(a_symbol == another_symbol).to eq(__)
+    expect(a_symbol == the_same_symbol).to eq(true)
+    expect(a_symbol == another_symbol).to eq(false)
   end
 
   it 'is a single, non-unique internal object' do
     a_symbol        = :a_symbol
     the_same_symbol = :a_symbol
 
-    expect(a_symbol.object_id == the_same_symbol.object_id).to eq(__)
+    expect(a_symbol.object_id == the_same_symbol.object_id).to eq(true)
   end
 
   it 'is created for every method name' do
     symbols_as_strings = Symbol.all_symbols.map { |symbol| symbol.to_s }
 
     # `split` is a method on String that we have used previously
-    expect(symbols_as_strings.include?('split')).to eq(__)
+    expect(symbols_as_strings.include?('split')).to eq(true)
   end
 
   MAGIC_NUMBER = 3
   it 'is created for every constant' do
     symbols_as_strings = Symbol.all_symbols.map { |symbol| symbol.to_s }
 
-    expect(symbols_as_strings.include?('MAGIC_NUMBER')).to eq(__)
+    expect(symbols_as_strings.include?('MAGIC_NUMBER')).to eq(true)
   end
 
   it 'can be created from a string' do
     string = 'helloWorld'
 
-    expect(string.to_sym).to eq(__)
+    expect(string.to_sym).to eq(:helloWorld)
   end
 
   it 'can be created with spaces' do
     # NB: Symbols traditionally are snake_case only (Please don't use spaces!)
     symbol = :"hello world"
 
-    expect(symbol.is_a?(Symbol)).to eq(__)
+    expect(symbol.is_a?(Symbol)).to eq(true)
   end
 
   it 'can be created with an underscore or a hyphen' do
@@ -52,43 +52,43 @@ RSpec.describe 'Symbols' do
     # NB: Symbols traditionally are snake_case only (Please don't use hyphens also!)
     hyphenated_symbol = :"hello-world"
 
-    expect(underscore_symbol.is_a?(Symbol)).to eq(__)
-    expect(hyphenated_symbol.is_a?(Symbol)).to eq(__)
+    expect(underscore_symbol.is_a?(Symbol)).to eq(true)
+    expect(hyphenated_symbol.is_a?(Symbol)).to eq(true)
   end
 
   it 'can be created with interpolated values' do
     value = 'world'
     symbol = :"hello #{value}"
 
-    expect(symbol).to eq(__)
+    expect(symbol).to eq(:'hello world')
   end
 
   it 'can be interpolated into a string' do
     symbol = :world
     string = "hello #{symbol}"
 
-    expect(string).to eq(__)
+    expect(string).to eq('hello world')
   end
 
   it 'is not a string' do
     symbol = :world
 
-    expect(symbol.is_a?(String)).to eq(__)
-    expect(symbol == 'ruby').to eq(__)
+    expect(symbol.is_a?(String)).to eq(false)
+    expect(symbol == 'ruby').to eq(false)
   end
 
   it "doesn't have string methods" do
     symbol = :world
 
-    expect(symbol.respond_to?(:reverse)).to eq(__)
-    expect(symbol.respond_to?(:split)).to eq(__)
+    expect(symbol.respond_to?(:reverse)).to eq(false)
+    expect(symbol.respond_to?(:split)).to eq(false)
   end
 
   it "can't be concatenated" do
-    expect { :hello + :world }.to raise_error(__)
+    expect { :hello + :world }.to raise_error(NoMethodError)
   end
 
   it 'can be created dynamically' do
-    expect(('hello' + 'world').to_sym).to eq(__)
+    expect(('hello' + 'world').to_sym).to eq(:helloworld)
   end
 end
