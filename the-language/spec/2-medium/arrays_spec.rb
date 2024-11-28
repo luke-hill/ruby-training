64e5b2163e078fd5418756109db62e297f331ad1
toby3carter <toby.carter@howsy.com>
2024-11-28 16:36:32 +0000

    This is the progress I have made so far on completing the-language 2-medium

--- the-language/spec/2-medium/arrays_spec.rb
+++ the-language/spec/2-medium/arrays_spec.rb
@@ -2,8 +2,8 @@ RSpec.describe 'Arrays' do
   it 'can be created like any other object' do
     empty = Array.new
 
-    expect(empty.class).to eq(__)
-    expect(empty.size).to eq(__)
+    expect(empty.class).to eq(Array)
+    expect(empty.size).to eq(0)
   end
 
   it 'can also be created using an array literal' do
@@ -13,60 +13,60 @@ RSpec.describe 'Arrays' do
     expect(array).to eq([1])
 
     array[1] = 2
-    expect(array).to eq([1, __])
+    expect(array).to eq([1, 2])
 
     # This is sometimes referred to as the shovel operator
     array << 333
-    expect(array).to eq(__)
+    expect(array).to eq([1, 2, 333])
   end
 
   it 'provides the subscript (index), operator for accessing elements' do
     array = ['New York', 'Paris', 'London', 'Milan']
 
-    expect(array[0]).to eq(__)
-    expect(array[3]).to eq(__)
+    expect(array[0]).to eq('New York')
+    expect(array[3]).to eq('Milan')
 
     # What do you thing will happen here?
-    expect(array[-1]).to eq(__)
-    expect(array[-3]).to eq(__)
+    expect(array[-1]).to eq('Milan')
+    expect(array[-3]).to eq('Paris')
   end
 
   it 'has methods for accessing the beginning and end of the array' do
     array = ['New York', 'Paris', 'London', 'Milan']
 
-    expect(array.first).to eq(__)
-    expect(array.last).to eq(__)
+    expect(array.first).to eq('New York')
+    expect(array.last).to eq('Milan')
   end
 
   it 'can be sliced using #slice with 2 arguments' do
     array = ['New York', 'Paris', 'London', 'Milan']
 
-    expect(array.slice(0, 1)).to eq(__)
-    expect(array.slice(0, 2)).to eq(__)
-    expect(array.slice(3, 3)).to eq(__)
-    expect(array.slice(2, 20)).to eq(__)
-    expect(array.slice(4, 0)).to eq(__)
-    expect(array.slice(4, 10)).to eq(__)
-    expect(array.slice(5, 0)).to eq(__)
-    expect(array.slice(1, 1)).to eq(__)
-    expect(array.slice(2, 1)).to eq(__)
+    expect(array.slice(0, 1)).to eq(['New York']) # Starts at index 0, takes 1 element
+    expect(array.slice(0, 2)).to eq(['New York', 'Paris']) # Starts at index 0, takes 2 elements
+    expect(array.slice(3, 3)).to eq(['Milan']) # Starts at index 3, but only 1 element exists
+    expect(array.slice(2, 20)).to eq(['London', 'Milan']) # Starts at index 2, but only 2 elements exist
+    expect(array.slice(4, 0)).to eq([]) # Index 4 is beyond bounds, returns empty array
+    expect(array.slice(4, 10)).to eq([]) # Index 4 is beyond bounds, returns nil
+    expect(array.slice(5, 0)).to eq(nil) # Index 5 is beyond bounds, returns nil
+    expect(array.slice(1, 1)).to eq(['Paris']) # Starts at index 1, takes 1 element
+    expect(array.slice(2, 1)).to eq(['London']) # Starts at index 2, takes 1 element
   end
 
   it 'is similar to a range' do
-    expect((1..5).class).to eq(__)
+    expect((1..5).class).to eq(Range)
     # Given these two items are not equal what do we expect to do with the tests below?
     expect([1, 2, 3, 4, 5]).not_to eq((1..5))
-    expect(__).to eq((1..5).to_a)
-    expect(__).to eq((1...5).to_a)
+    expect([1, 2, 3, 4, 5]).to eq((1..5).to_a)
+    expect([1, 2, 3, 4]).to eq((1...5).to_a)
   end
 
   it 'can be sliced by a range' do
     array = ['New York', 'Paris', 'London', 'Milan']
 
-    expect(__).to eq(array[0..2])
-    expect(__).to eq(array[0...2])
-    expect(__).to eq(array[2..-1])
-    expect(__).to eq(array[-2..1])
+    expect(['New York', 'Paris', 'London']).to eq(array[0..2])
+    expect(['New York', 'Paris']).to eq(array[0...2])
+    expect(['London', 'Milan']).to eq(array[2..-1]) # starts at index 2 and goes to the last element
+    expect([]).to eq(array[-2..1]) # is an invalid range (end index before start)
   end
 
   it 'can be used as a stack' do
@@ -74,38 +74,38 @@ RSpec.describe 'Arrays' do
 
     array.push('on the end')
 
-    expect(array).to eq(__)
+    expect(array).to eq([1, 2, 'on the end'])
 
     value = array.pop
-    expect(value).to eq(__)
-    expect(array).to eq(__)
+    expect(value).to eq('on the end')
+    expect(array).to eq([1, 2])
   end
 
   it 'can be used as a queue' do
     array = [:first, :second]
     array.push(:third)
-    expect(array).to eq(__)
+    expect(array).to eq([:first, :second, :third])
 
     value = array.shift
-    expect(value).to eq(__)
-    expect(array).to eq(__)
+    expect(value).to eq(:first)
+    expect(array).to eq([:second, :third])
 
     # Sometimes you need to force things to the front of a queue
     # A bit like if someone was pushing in at the supermarket!
     array.unshift(value)
 
-    expect(array).to eq(__)
+    expect(array).to eq([:first, :second, :third])
   end
 
   it 'has short-hand notation for arrays of strings or symbols' do
     string_array = ['Welcome', 'to', 'the', 'joinery']
     simplified_string_array = %w[Welcome to the joinery]
 
-    expect(string_array == simplified_string_array).to eq(__)
+    expect(string_array == simplified_string_array).to eq(true)
 
     symbol_array = [:foo, :bar, :baz, :bay]
     simplified_symbol_array = %i[foo bar baz bay]
 
-    expect(symbol_array == simplified_symbol_array).to eq(__)
+    expect(symbol_array == simplified_symbol_array).to eq(true)
   end
 end
--- the-language/spec/2-medium/class_methods_spec.rb
+++ the-language/spec/2-medium/class_methods_spec.rb
@@ -4,24 +4,24 @@ RSpec.describe 'Class methods' do
 
   it 'all instances of a class are objects' do
     percy = Cat.new
-    expect(percy.is_a?(Object)).to eq(__)
+    expect(percy.is_a?(Object)).to eq(true)
   end
 
   it 'all classes (constructors), are classes' do
-    expect(Cat.is_a?(Class)).to eq(__)
+    expect(Cat.is_a?(Class)).to eq(true)
   end
 
   it 'classes are objects too' do
-    expect(Cat.is_a?(Object)).to eq(__)
+    expect(Cat.is_a?(Object)).to eq(true)
   end
 
   it 'instances have methods' do
     percy = Cat.new
-    expect(percy.methods.size > __).to be true
+    expect(percy.methods.size > 0).to be true
   end
 
   it 'classes also have methods' do
-    expect(Cat.methods.size > __).to be true
+    expect(Cat.methods.size > 0).to be true
   end
 
   it 'is possible to define a method on an individual object' do
@@ -30,7 +30,7 @@ RSpec.describe 'Class methods' do
     def percy.wag
       :percys_wag
     end
-    expect(percy.wag).to eq(__)
+    expect(percy.wag).to eq(:percys_wag)
   end
 
   it 'does not affect other objects (instances), when defining singleton methods' do
@@ -40,7 +40,7 @@ RSpec.describe 'Class methods' do
       :percys_wag
     end
 
-    expect { not_percy.wag }.to raise_error(__)
+    expect { not_percy.wag }.to raise_error(NoMethodError)
   end
 
   class TabbyCat
@@ -54,13 +54,13 @@ RSpec.describe 'Class methods' do
   end
 
   it 'is possible to define singleton methods on classes (remember they are objects)' do
-    expect(TabbyCat.wag).to eq(__)
+    expect(TabbyCat.wag).to eq(:class_level_wag)
   end
 
   it 'is possible to have both a class and instance method identically named (they are independent)' do
     percy = TabbyCat.new
-    expect(percy.wag).to eq(__)
-    expect(TabbyCat.wag).to eq(__)
+    expect(percy.wag).to eq(:instance_level_wag)
+    expect(TabbyCat.wag).to eq(:class_level_wag)
   end
 
   class TawnyCat
@@ -74,8 +74,8 @@ RSpec.describe 'Class methods' do
   it 'does not share instance variables between classes and instances' do
     percy = TawnyCat.new
     percy.name = 'Percy'
-    expect(percy.name).to eq(__)
-    expect(TawnyCat.name).to eq(__)
+    expect(percy.name).to eq('Percy')
+    expect(TawnyCat.name).to eq(nil)
   end
 
   LAST_EXPRESSION_IN_CLASS_STATEMENT = class GingerCat
@@ -83,7 +83,7 @@ RSpec.describe 'Class methods' do
                                        end
 
   it 'returns the last expression inside a class statement (when defining a class)' do
-    expect(LAST_EXPRESSION_IN_CLASS_STATEMENT).to eq(__)
+    expect(LAST_EXPRESSION_IN_CLASS_STATEMENT).to eq(21)
   end
 
   SELF_INSIDE_OF_CLASS_STATEMENT = class GingerCat
@@ -91,7 +91,7 @@ RSpec.describe 'Class methods' do
                                    end
 
   it 'uses self to refer to the class (not an instance of it), when inside the definition' do
-    expect(GingerCat == SELF_INSIDE_OF_CLASS_STATEMENT).to eq(__)
+    expect(GingerCat == SELF_INSIDE_OF_CLASS_STATEMENT).to eq(true)
   end
 
   class BrownCat
@@ -101,7 +101,7 @@ RSpec.describe 'Class methods' do
   end
 
   it 'is possible to use self to define a class method' do
-    expect(BrownCat.class_method).to eq(__)
+    expect(BrownCat.class_method).to eq(:another_way_to_write_class_methods)
   end
 
   class BlackCat
@@ -113,7 +113,7 @@ RSpec.describe 'Class methods' do
   end
 
   it 'has a third way to define a class method' do
-    expect(BlackCat.another_class_method).to eq(__)
+    expect(BlackCat.another_class_method).to eq(:still_another_way)
   end
 
   # THINK ABOUT IT:
@@ -139,7 +139,7 @@ RSpec.describe 'Class methods' do
       self.class.another_class_method
     end
 
-    expect(percy.instance_method).to eq(__)
+    expect(percy.instance_method).to eq(:still_another_way)
   end
 
   # NOTE: Reopening BlackCat class
@@ -153,6 +153,6 @@ RSpec.describe 'Class methods' do
   it 'has a better way to call class methods from instance methods' do
     percy = BlackCat.new
 
-    expect(percy.call_class_method_from_instance_method).to eq(__)
+    expect(percy.call_class_method_from_instance_method).to eq(:still_another_way)
   end
 end
