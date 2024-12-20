b651bf08cbf367e84564c97a96448b8d7aab8a5a
Luke Hill <lukehill_uk@hotmail.com>
2024-12-20 12:07:21 +0000

    Add initial files for game of life

---
+++ game_of_life/lib/game_of_life/game.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module GameOfLife
+  class Game
+  end
+end
---
+++ game_of_life/spec/game_of_life/game_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'game_of_life/game'
+
+# Change this test as it basically does nothing!
+RSpec.describe GameOfLife::Game do
+  it 'works' do
+    expect(true).to be_truthy
+  end
+end
