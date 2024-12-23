34d503aa8cdf2f8212ce54bc49fa8b250f1bfde3
Luke Hill <lukehill_uk@hotmail.com>
2024-12-23 16:03:41 +0000

    Use cell_at method

--- game_of_life/partial_solutions/40_percent/lib/game_of_life/game.rb
+++ game_of_life/partial_solutions/40_percent/lib/game_of_life/game.rb
@@ -30,49 +30,49 @@ module GameOfLife
     def top_left_neighbour(x, y)
       return nil if y.zero? || x.zero?
 
-      cells[y - 1][x - 1]
+      cell_at(x - 1, y - 1)
     end
 
     def top_neighbour(x, y)
       return nil if y.zero?
 
-      cells[y - 1][x]
+      cell_at(x, y - 1)
     end
 
     def top_right_neighbour(x, y)
       return nil if y.zero? || x == grid_size
 
-      cells[y - 1][x + 1]
+      cell_at(x + 1, y - 1)
     end
 
     def left_neighbour(x, y)
       return nil if x.zero?
 
-      cells[y][x - 1]
+      cell_at(x - 1, y)
     end
 
     def right_neighbour(x, y)
       return nil if x == grid_size
 
-      cells[y][x + 1]
+      cell_at(x + 1, y)
     end
 
     def bottom_left_neighbour(x, y)
       return nil if y == grid_size || x.zero?
 
-      cells[y + 1][x - 1]
+      cell_at(x - 1, y + 1)
     end
 
     def bottom_neighbour(x, y)
       return nil if y == grid_size
 
-      cells[y + 1][x]
+      cell_at(x, y + 1)
     end
 
     def bottom_right_neighbour(x, y)
       return nil if y == grid_size || x == grid_size
 
-      cells[y + 1][x + 1]
+      cell_at(x + 1, y + 1)
     end
   end
 end
