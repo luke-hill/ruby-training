34d829ef58e1276a660f2aa7c477c9215fbb08f9
Luke Hill <lukehill_uk@hotmail.com>
2025-01-06 15:16:31 +0000

    Update all standard holding code to ruby 3.4.1

--- nim/spec/nim/game_spec.rb
+++ nim/spec/nim/game_spec.rb
@@ -4,7 +4,7 @@ require 'nim/game'
 
 # Change this test as it basically does nothing!
 RSpec.describe Nim::Game do
-  it 'works' do
+  it 'passes as a sample test' do
     expect(true).to be_truthy
   end
 end
