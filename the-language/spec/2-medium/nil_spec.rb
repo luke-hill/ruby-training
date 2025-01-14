092cc4a82876688d24ece797c65836261635608f
toby3carter <toby.carter@howsy.com>
2025-01-14 14:37:08 +0000

    Here is my completed version of the nil spec file

--- the-language/spec/2-medium/nil_spec.rb
+++ the-language/spec/2-medium/nil_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe 'Nil in ruby' do
   it 'is an object' do
-    expect(nil.is_a?(Object)).to eq(__), 'unlike NULL in some other languages'
+    expect(nil.is_a?(Object)).to eq(true), 'unlike NULL in some other languages'
   end
 
   it "doesn't throw null pointer exceptions" do
@@ -11,19 +11,19 @@ RSpec.describe 'Nil in ruby' do
       nil.a_method_that_does_not_exist
     rescue NoMethodError => e
       # What sort of exception is thrown
-      expect(e.class).to eq(__)
+      expect(e.class).to eq(NoMethodError)
 
       # What extra information does ruby give us?
       # You should replace __ here with part of the message
       # / / is regex and just means that the item inside is "contained"
-      expect(e.message).to match(/__/)
+      expect(e.message).to match(/a_method_that_does_not_exist/)
     end
   end
 
   it 'has a few methods defined on it' do
-    expect(nil.nil?).to eq(__)
-    expect(nil.to_s).to eq(__)
-    expect(nil.inspect).to eq(__)
+    expect(nil.nil?).to eq(true)
+    expect(nil.to_s).to eq("")
+    expect(nil.inspect).to eq("nil")
   end
 
   context 'RSpec matchers' do
@@ -41,8 +41,8 @@ RSpec.describe 'Nil in ruby' do
 
     it 'can match objects that respond to nil?' do
       # Make sure you use the objects above
-      expect(__).to be_nil
-      expect(__).not_to be_nil
+      expect(NilObject.new).to be_nil
+      expect(NotNilObject.new).not_to be_nil
     end
   end
 end
