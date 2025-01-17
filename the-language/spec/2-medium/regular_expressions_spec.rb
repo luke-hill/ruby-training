2ed017b0ab58b2554b331cbc7983965cfdead3e0
toby3carter <toby.carter@howsy.com>
2025-01-17 15:34:49 +0000

    progress made on regular expressions spec file

--- the-language/spec/2-medium/regular_expressions_spec.rb
+++ the-language/spec/2-medium/regular_expressions_spec.rb
@@ -1,74 +1,74 @@
 RSpec.describe 'Regular expressions' do
   it 'is a type of regexp' do
-    expect(/pattern/.class).to eq(__)
+    expect(/pattern/.class).to eq(Regexp)
   end
 
   it 'can fetch the first instance of a pattern that is found in a string' do
-    expect('a string that matches'[/match/]).to eq(__)
+    expect('a string that matches'[/match/]).to eq("match")
   end
 
   it 'returns nil when the match fails' do
-    expect('a string that matches'[/fails/]).to eq(__)
+    expect('a string that matches'[/fails/]).to eq(nil)
   end
 
   it 'uses a ? on its own to signify "optional" (0 or 1 of the preceding item)' do
-    expect('abcd'[/ab?/]).to eq(__)
-    expect('abcd'[/ae?/]).to eq(__)
+    expect('abcd'[/ab?/]).to eq("ab")
+    expect('abcd'[/ae?/]).to eq("a")
   end
 
   it 'uses + to mean one or more (of the preceding group)' do
-    expect('abcccd'[/ac+/]).to eq(__)
-    expect('abcccd'[/bc+/]).to eq(__)
+    expect('abcccd'[/ac+/]).to eq(nil)
+    expect('abcccd'[/bc+/]).to eq("bccc")
   end
 
   it 'uses * to mean zero or more (of the preceding group)' do
-    expect('abbcccdddd'[/ab*/]).to eq(__)
-    expect('abbcccdddd'[/az*/]).to eq(__)
-    expect('abbcccdddd'[/z*/]).to eq(__)
+    expect('abbcccdddd'[/ab*/]).to eq("abb")
+    expect('abbcccdddd'[/az*/]).to eq("a")
+    expect('abbcccdddd'[/z*/]).to eq("")
   end
 
   it 'matches from the left first' do
-    expect('acdz az'[/az*/]).to eq(__)
+    expect('acdz az'[/az*/]).to eq("a")
   end
 
   it 'can use character classes to provide options' do
     words = ['cat', 'sat', 'mat']
-    expect(words.select { |word| word[/[cm]at/] }).to eq(__)
+    expect(words.select { |word| word[/[cm]at/] }).to eq(["cat", "mat"])
   end
 
   it 'uses \d as a shortcut for a digit character class' do
-    expect('3 is the magic number'[/[0123456789]/]).to eq(__)
-    expect('3 is the magic number'[/\d/]).to eq(__)
+    expect('3 is the magic number'[/[0123456789]/]).to eq("3")
+    expect('3 is the magic number'[/\d/]).to eq("3")
   end
 
   it 'allows ranges in character classes' do
-    expect('3 is the magic number'[/[0-9]/]).to eq(__)
+    expect('3 is the magic number'[/[0-9]/]).to eq("3")
   end
 
   it 'uses \s as the shortcut for white space' do
-    expect("space: \t\n"[/\s+/]).to eq(__)
+    expect("space: \t\n"[/\s+/]).to eq(" \t\n")
   end
 
   it 'uses \w as the shortcut for word characters' do
-    expect('magic_number_1 = 3'[/[a-zA-Z0-9_]+/]).to eq(__)
-    expect('magic_number_1 = 3'[/\w+/]).to eq(__)
+    expect('magic_number_1 = 3'[/[a-zA-Z0-9_]+/]).to eq("magic_number_1")
+    expect('magic_number_1 = 3'[/\w+/]).to eq("magic_number_1")
   end
 
   it 'uses . as a shortcut for any character, except a new line' do
-    expect("abc\n123"[/a.+/]).to eq(__)
+    expect("abc\n123"[/a.+/]).to eq("abc")
   end
 
   it 'can negate a character class' do
-    expect('3 is the magic number'[/[^0-9]+/]).to eq(__)
+    expect('3 is the magic number'[/[^0-9]+/]).to eq(" is the magic number")
   end
 
   it 'uses capitals to negate shortcut characters' do
     # Capitals are less commonly used as they're not as comprehensible
-    expect('3 is the magic number'[/\D+/]).to eq(__)
-    expect("space: \t\n"[/\S+/]).to eq(__)
+    expect('3 is the magic number'[/\D+/]).to eq(" is the magic number")
+    expect("space: \t\n"[/\S+/]).to eq("space:")
     # ... a programmer would most likely do
-    expect('magic_number_1 = 3'[/[^a-zA-Z0-9_]+/]).to eq(__)
-    expect('magic_number_1 = 42'[/\W+/]).to eq(__)
+    expect('magic_number_1 = 3'[/[^a-zA-Z0-9_]+/]).to eq(" = ")
+    expect('magic_number_1 = 42'[/\W+/]).to eq(" = ")
   end
 
   it 'uses \A to mark the start of the string' do
