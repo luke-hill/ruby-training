5a558ebb599ef5f513184fbe3f44692dfd335d48
Luke Hill <lukehill_uk@hotmail.com>
2025-02-06 14:21:26 +0000

    Tweak to class definitions

--- the-language/spec/2-medium/constants_spec.rb
+++ the-language/spec/2-medium/constants_spec.rb
@@ -1,5 +1,6 @@
 C = 'top level'
 
+# Constants at existing class vs top level class
 class More
   C = 'nested'
 
@@ -9,6 +10,44 @@ class More
 
   def self.double_colon
     ::C
+  end
+end
+
+# Constants in a class vs nested class
+class Animal
+  LEGS = 4
+
+  class NestedAnimal
+    def legs_in_nested_animal
+      LEGS
+    end
+  end
+end
+
+# Constants in a child class
+class Reptile < Animal
+  def legs_in_reptile
+    LEGS
+  end
+end
+
+# Constants in a nested class that also inherits
+class Zoo
+  LEGS = 2
+
+  class Bird < Animal
+    def legs_in_bird
+      LEGS
+    end
+  end
+end
+
+# Constants in a inheriting class that previously inherited a definition
+class Zoo
+  class Oyster < Animal
+    def legs_in_oyster
+      LEGS
+    end
   end
 end
 
@@ -26,51 +65,17 @@ describe 'Constants in ruby are different' do
     expect(::More::C).to eq(__)
   end
 
-  class Animal
-    LEGS = 4
-
-    class NestedAnimal
-      def legs_in_nested_animal
-        LEGS
-      end
-    end
-  end
-
   it 'inherits constants from enclosing classes' do
     expect(Animal::NestedAnimal.new.legs_in_nested_animal).to eq(__)
-  end
-
-  class Reptile < Animal
-    def legs_in_reptile
-      LEGS
-    end
   end
 
   it 'inherits constants when subclassed from the parent class' do
     expect(Reptile.new.legs_in_reptile).to eq(__)
   end
 
-  class Zoo
-    LEGS = 2
-
-    class Bird < Animal
-      def legs_in_bird
-        LEGS
-      end
-    end
-  end
-
   context 'with an inherited class and an enclosing class' do
     it 'can add a new overridden constant (from the enclosing class), in a subclass' do
       expect(Zoo::Bird.new.legs_in_bird).to eq(__)
-    end
-
-    class Zoo
-      class Oyster < Animal
-        def legs_in_oyster
-          LEGS
-        end
-      end
     end
 
     # Remember that we have already defined what Zoo::LEGS was earlier
