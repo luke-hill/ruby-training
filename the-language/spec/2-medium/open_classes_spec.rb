1615817b09fd91622e960394278ba089dbb858de
Luke Hill <lukehill_uk@hotmail.com>
2025-02-24 11:52:07 +0000

    Add overwriting open class method logic

--- the-language/spec/2-medium/open_classes_spec.rb
+++ the-language/spec/2-medium/open_classes_spec.rb
@@ -33,4 +33,17 @@ RSpec.describe 'Classes can be opened in ruby' do
     expect(1.even_number?).to eq(__)
     expect(2.even_number?).to eq(__)
   end
+
+  # Open the existing Hamster class and rewrite a method.
+  class Hamster
+    def nibble
+      'i am nibbling differently now'
+    end
+  end
+
+  it 'nibbles differently' do
+    hammy = Hamster.new
+    expect(hammy.nibble).to eq(__)
+    expect(hammy.eat).to eq(__)
+  end
 end
