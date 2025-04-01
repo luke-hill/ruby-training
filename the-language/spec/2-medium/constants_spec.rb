295e1b82a6723c8d9b746c4f71c285c932802670
Luke Hill <20105237+luke-hill@users.noreply.github.com>
2025-04-01 16:03:27 +0100

    Refactor/constants (#146)
    Closes #143

--- the-language/spec/2-medium/constants_spec.rb
+++ the-language/spec/2-medium/constants_spec.rb
@@ -1,5 +1,6 @@
 C = 'top level'
 
+# Constants at existing class vs top level class
 class More
   C = 'nested'
 
@@ -16,10 +17,57 @@ class More
   end
 end
 
-describe 'Constants in ruby' do
+# Constants in a class vs nested class
+class Animal
+  LEGS = 4
+
+  class NestedAnimal
+    def legs_in_nested_animal
+      LEGS
+    end
+  end
+
+  class Millipede
+    LEGS = 1000
+
+    def legs_in_millipede
+      LEGS
+    end
+  end
+end
+
+# Constants in a child class
+class Reptile < Animal
+  def legs_in_reptile
+    LEGS
+  end
+end
+
+# Constants in a nested class that also inherits
+class Zoo
+  LEGS = 2
+
+  class Bird < Animal
+    def legs_in_bird
+      LEGS
+    end
+  end
+end
+
+# Constants in a inheriting class that previously inherited a definition
+class Zoo
+  class Oyster < Animal
+    def legs_in_oyster
+      LEGS
+    end
+  end
+end
+
+describe 'Constants' do
   # Constants in ruby act quite differently to other languages. Predominantly they are different
   # because they can be instantiated at any point in the life of the program, as such they are not
   # really "constant" because they can be modified / altered
+
   it 'references local constants' do
     expect(More.local).to eq(__)
   end
@@ -38,51 +86,21 @@ describe 'Constants in ruby' do
     expect(instance.local).to eq(__)
   end
 
-  class Animal
-    LEGS = 4
-
-    class NestedAnimal
-      def legs_in_nested_animal
-        LEGS
-      end
-    end
-  end
-
   it 'inherits constants from enclosing classes' do
     expect(Animal::NestedAnimal.new.legs_in_nested_animal).to eq(__)
   end
 
-  class Reptile < Animal
-    def legs_in_reptile
-      LEGS
-    end
+  it 'ignores inherited constants if a more local definition exists' do
+    expect(Animal::Millipede.new.legs_in_millipede).to eq(__)
   end
 
   it 'inherits constants when subclassed from the parent class' do
     expect(Reptile.new.legs_in_reptile).to eq(__)
   end
 
-  class Zoo
-    LEGS = 2
-
-    class Bird < Animal
-      def legs_in_bird
-        LEGS
-      end
-    end
-  end
-
   context 'with an inherited class AND an enclosing class' do
     it 'can add a new overridden constant (from the enclosing class), in a subclass' do
       expect(Zoo::Bird.new.legs_in_bird).to eq(__)
-    end
-
-    class Zoo
-      class Oyster < Animal
-        def legs_in_oyster
-          LEGS
-        end
-      end
     end
 
     # Remember that we have already defined what Zoo::LEGS was earlier
