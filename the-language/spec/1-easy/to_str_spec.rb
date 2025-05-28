96d0860b238f5787e3c7be2cd8d7b8ec0a610ead
Luke Hill <lukehill_uk@hotmail.com>
2025-05-28 11:38:10 +0100

    Fix Leaky constants in to_str_spec

--- the-language/spec/1-easy/to_str_spec.rb
+++ the-language/spec/1-easy/to_str_spec.rb
@@ -1,40 +1,39 @@
 # frozen_string_literal: true
 
 RSpec.describe 'Converting items to Strings' do
-  class NotLikeAString
-    def to_s
-      'non-string-like'
+  let(:not_like_a_string) do
+    Class.new do
+      def to_s
+        'non-string-like'
+      end
     end
   end
+  let(:like_a_string) do
+    Class.new do
+      def to_s
+        'string-like'
+      end
 
-  class LikeAString
-    def to_s
-      'string-like'
-    end
-
-    def to_str
-      to_s
+      def to_str
+        to_s
+      end
     end
   end
 
   it 'returns a string representation using `#to_s`' do
-    not_like_a_string = NotLikeAString.new
-
-    expect(not_like_a_string.to_s).to eq(__)
+    expect(not_like_a_string.new.to_s).to eq(__)
   end
 
   it 'cannot use normal objects in place of strings by using `#to_s`' do
-    expect { File.exist?(NotLikeAString.new) }.to raise_error(__)
+    expect { File.exist?(not_like_a_string.new) }.to raise_error(__)
   end
 
   it 'also returns a string representation using `#to_str`' do
-    like_a_string = LikeAString.new
-
-    expect(like_a_string.to_str).to eq(__)
+    expect(like_a_string.new.to_str).to eq(__)
   end
 
   it 'can be used in place of a string by using `#to_str`' do
-    expect(File.exist?(LikeAString.new)).to eq(__)
+    expect(File.exist?(like_a_string.new)).to eq(__)
   end
 
   def acts_like_a_string?(string)
@@ -43,7 +42,7 @@ RSpec.describe 'Converting items to Strings' do
   end
 
   it 'can check for `#to_str` as a normal method in user-defined code' do
-    expect(acts_like_a_string?(NotLikeAString.new)).to eq(__)
-    expect(acts_like_a_string?(LikeAString.new)).to eq(__)
+    expect(acts_like_a_string?(not_like_a_string.new)).to eq(__)
+    expect(acts_like_a_string?(like_a_string.new)).to eq(__)
   end
 end
