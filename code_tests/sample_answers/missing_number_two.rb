9f672dbc4e81025ec5d75359eb83bb8dfc9d0045
Luke Hill <lukehill_uk@hotmail.com>
2026-04-20 14:16:21 +0100

    Update performance of missing number examples to use ruby 4 isms

--- code_tests/sample_answers/missing_number_two.rb
+++ code_tests/sample_answers/missing_number_two.rb
@@ -4,12 +4,12 @@
 class MissingNumber
   def detect(input_array)
     # Catch by checking whether every input is an integer
-    raise ArgumentError unless input_array.all? { |e| e.is_a?(Integer) }
+    raise ArgumentError unless input_array.all?(Integer)
 
     # Only count along the array, summing all values
-    actual_sum = input_array.inject(0) { |sum, value| sum + value }
+    actual_sum = input_array.sum
     # Count along a new theoretical array, summing all values
-    theoretical_sum = (1..input_array.length + 1).inject(0) { |sum, value| sum + value }
+    theoretical_sum = (1..input_array.length + 1).sum
 
     # Mathematical operations
     if (theoretical_sum - actual_sum).zero?
